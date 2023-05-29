24e6c4b815e353666849514ff595f0cdd2862f63
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-05-29 22:00:30 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -26,7 +26,7 @@ try{
    // if the user exists
     if($stmt->rowCount()){
         
-        echo '<meta http-equiv="refresh" content="0; url=html/Accueil.php" />';
+        echo '<meta http-equiv="refresh" content="0; url= Accueil.php" />';
         //header("Location: html\Accueil.html");
         exit;
     }
