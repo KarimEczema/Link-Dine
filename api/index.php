31f705e9b27012c93807bca80a3e459f160fe1db
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-05-29 20:40:32 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,20 +2,17 @@
 <html>
 <body>
 
-<h2>Menu de traitement des employés</h2>
+<h2>Login</h2>
 
-<p>Choisissez une option de traitement :</p>
-
-<ul>
-  <li><a href="3TraitementA.php">Ajouter un nouvel employé et afficher tous les employés</a></li>
-  <li><a href="3TraitementB.php">Supprimer un employé et afficher tous les employés</a></li>
-  <li><a href="3TraitementC.php">Afficher tous les employés par ordre croissant de salaire</a></li>
-  <li><a href="3TraitementD.php">Afficher tous les employés par ordre décroissant d'ID</a></li>
-  <li><a href="3TraitementE.php">Afficher le salaire minimum et maximum</a></li>
-  <li><a href="3TraitementF.php">Afficher le salaire moyen et le nombre total d'employés</a></li>
-  <li><a href="3TraitementG.php">Compte le nombre de l’ID_Travail distinct dans la table</a></li>
-  <li><a href="3TraitementH.php">Affiche tous les champs de tous les employés dont le patron a un ID de 100</a></li>
-</ul>
+<form method="post" action="">
+  Username:<br>
+  <input type="text" name="username">
+  <br>
+  Password:<br>
+  <input type="password" name="password">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
 </body>
 </html>
@@ -30,27 +27,33 @@ $password = "Y4vuPQm2xyTl";
 
 $dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
 
-try {
-  // create a PostgreSQL database connection
-  $conn = new PDO($dsn);
-
-  // display a message if connected to the PostgreSQL successfully
-  if($conn){
-      echo "Connected to the <strong>$dbname</strong> database successfully!";
-      
-      // SQL script to select and order data
-      $sql = "SELECT * FROM test_table ORDER BY name ASC";
-
-      // prepare and execute the SQL script
-      $result = $conn->query($sql);
-
-      // loop through the result set and display the data
-      while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-          echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Email: " . $row['email'] . "<br>";
-      }
-  }
-} catch (PDOException $e) {
-  // report error message
-  echo $e->getMessage();
+try{
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+    
+    // if form is submitted
+    if($_POST){
+        // query to check if username and password are correct
+        $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+        $stmt = $conn->prepare($sql);
+        
+        // bind parameters and execute
+        $stmt->bindParam(':username', $_POST['username']);
+        $stmt->bindParam(':password', $_POST['password']);
+        $stmt->execute();
+        
+        // if the user exists
+        if($stmt->rowCount()){
+            echo "Logged in successfully!";
+        }
+        else{
+            echo "Invalid username or password!";
+        }
+    }
+    
+}catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
 }
 ?>
+
