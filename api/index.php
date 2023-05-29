24474bd9151d4f57627b44d3aa18a5073ce68ef8
KarimEczema <elhousnyadel@gmail.com>
2023-05-29 20:25:36 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -21,35 +21,36 @@
 </html>
 
 <?php
-$host = "your_host_here";
-$port = "your_port_here";
-$dbname = "your_database_name_here";
-$user = "your_username_here";
-$password = "your_password_here";
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
+
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
 
 try {
-    // create a PostgreSQL database connection
-    $conn = new PDO($dsn);
+  // create a PostgreSQL database connection
+  $conn = new PDO($dsn);
 
-    // display a message if connected to the PostgreSQL successfully
-    if($conn){
-        echo "Connected to the <strong>$dbname</strong> database successfully!";
-        
-        // SQL script to select and order data
-        $sql = "SELECT * FROM test_table ORDER BY name ASC";
+  // display a message if connected to the PostgreSQL successfully
+  if($conn){
+      echo "Connected to the <strong>$dbname</strong> database successfully!";
+      
+      // SQL script to select and order data
+      $sql = "SELECT * FROM test_table ORDER BY name ASC";
 
-        // prepare and execute the SQL script
-        $result = $conn->query($sql);
+      // prepare and execute the SQL script
+      $result = $conn->query($sql);
 
-        // loop through the result set and display the data
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Email: " . $row['email'] . "<br>";
-        }
-    }
+      // loop through the result set and display the data
+      while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Email: " . $row['email'] . "<br>";
+      }
+  }
 } catch (PDOException $e) {
-    // report error message
-    echo $e->getMessage();
+  // report error message
+  echo $e->getMessage();
 }
 ?>
