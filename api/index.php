f8f6e506cff560f4a220e9a219ce10572f2a25bd
KarimEczema <elhousnyadel@gmail.com>
2023-05-29 20:17:17 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -36,7 +36,7 @@ try{
     
     // display a message if connected to the PostgreSQL successfully
     if($conn){
-        echo "Connected to the <strong>$db</strong> database successfully!";
+        echo "Connected to the <strong>$dbname</strong> database successfully!";
     }
 }catch (PDOException $e){
     // report error message
