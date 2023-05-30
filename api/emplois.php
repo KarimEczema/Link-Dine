b1e49438a974e2587dddc6251c0b3e9acaf6d124
GauthierMallard <gauthier.mallard@edu.ece.fr>
2023-05-30 16:28:17 +0200

    page emploi fonctionnelle

--- api/emplois.php
+++ api/emplois.php
@@ -1,124 +1,95 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>ECE-in</title>
-<meta charset="utf-8" />
-<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
- Chargement du JavaScript de Bootstrap
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
-<script type="text/javascript">
-	$(document).ready(function() {
-		var $img = $('#carrousel img');
-		var max = $img.length;
-		var i = 0; // compteur
-		$img.css('margin-left','0').css('display', 'none'); //on cache les images
-		$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-		$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-		$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-		$img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-		//si on clique sur « next » ou « > »
-		$('.next').click(function () { // image suivante
-			 i+=4; // on incrémente le compteur
-			 if (i < max-4) {
-			 i = i+4;
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
-			 i = 0;
-			 }
-		 });
-		//si on clique sur « prev » ou « < »
-		 $('.prev').click(function () { // groupe des images précédentes
-			 i-=4; // on décrémente le compteur
-			 if (i >= 0) {
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-			 } else {
-			 i = 0;
-			 }
-		 });
-		function slideImg() {
-			 setTimeout(function() {
-			 $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','0.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','0.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','1s');
-			 if (i < max-4) {
-			i = i+4;
-			 } else {
-			i = 0;
-			 }
-			 $img.css('margin-left','0').css('display', 'none');
-			 $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','1.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','1.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','2s');
-			slideImg();
-			}, 4000);
-		}
-		slideImg();
-	});
-</script>
+    <meta charset="utf-8">
+    <title>TD JQuery</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <link href="css/emploi.css" rel="stylesheet" type="text/css"/>
+
+
 </head>
 
-<link href="css/accueil.css" rel="stylesheet" type="text/css"/>
+<script type="text/javascript">
+    function textecache(ntexte){
+        var span =document.getElementById(ntexte);
+        if(span.style.display === "none")
+        {
+            span.style.display="inline";
+        }
+        else
+        {
+            span.style.display="none";
+        }
+    }
+</script>
+
+
 <body>
-	<header>
-		<div class="container-fluid">
-			<div class="row">
-				 <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
-				 <div class="col-sm" ><img src="images/LogECE.png" width="121" height="49.5"></div>
-			</div>
-		</div>
-	</header>
-
-	<nav class = "navigation">
-		<div class="container-fluid">
-				<div class="row">
-					 <div class="col-sm-2"><a href="accueil.html" style = "border : solid; color: black; padding:2px">Accueil</a></div>
-					 <div class="col-sm-2"><a href="Reseau.html" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div>
-					 <div class="col-sm-2"><a href="profil.html" style = "border : solid; color: black; padding:2px">Vous</a></div>
-					 <div class="col-sm-2"><a href="notifs.html" style = "border : solid; color: black; padding:2px">Notifications</a></div>
-					 <div class="col-sm-2"><a href="messages.html" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
-					 <div class="col-sm-2"><a href="emplois.html" style = "border : solid; color: black; padding:2px">Emplois</a></div>
-				</div>
-		</div>
-	</nav>
-
-
-
-	<nav class = "section">
-		<div id = "Emplois">
-			<h5 style = "text-align : center; color:red"> Offres d'emploi</h5>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
+            <div class="col-sm" ><img src="LogECE.png" width="121" height="49.5"></div>
         </div>
+    </div>
+</header>
 
-        <div class="scroll-container">
-          <div class="scroll-page" id="page-1">1</div>
-          <div class="scroll-page" id="page-2">2</div>
-          <div class="scroll-page" id="page-3">3</div>
+<nav class = "navigation">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2"><a href="accueil.html" style = "border : solid; color: black; padding:2px">Accueil</a></div>
+            <div class="col-sm-2"><a href="Reseau.html" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div>
+            <div class="col-sm-2"><a href="profil.html" style = "border : solid; color: black; padding:2px">Vous</a></div>
+            <div class="col-sm-2"><a href="notifs.html" style = "border : solid; color: black; padding:2px">Notifications</a></div>
+            <div class="col-sm-2"><a href="messages.html" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
+            <div class="col-sm-2"><a href="emplois.html" style = "border : solid; color: black; padding:2px">Emplois</a></div>
         </div>
+    </div>
+</nav>
 
 
-	</nav>
 
-	<nav class ="CV">
-	</nav>
+<nav class = "section">
+    <div id = "Emplois">
+        <h5 style = "text-align : center; color:red; border: 3px solid black; border-radius: 5%; padding : 3px;"> Offres d'emploi</h5>
+    </div>
+
+    <div class="scroll-container">
+        <div class="scroll-page" id="formation-1">
+            <h5><B>Intitulé du poste</B>- Employeur </h5>
+            <h6>Type de contrat </h6> <br>
+            <h6>Description du poste <button type="button" onclick="textecache('span_text1');">...</button> </h6>
+            <span id="span_text1" style="display: none";>Suite de la description trop longue</span>
+            <h6>Salaire</h6>
+        </div>
+        <div class="scroll-page" id="formation-2">
+            <h5><B>Intitulé du poste</B>- Employeur </h5>
+            <h6>Type de contrat </h6> <br>
+            <h6>Description du poste <button type="button" onclick="textecache('span_text2');">...</button> </h6>
+            <span id="span_text2" style="display: none";>Suite de la description trop longue</span>
+            <h6>Salaire</h6>
+        </div>
+        <div class="scroll-page" id="formation-3">
+            <h5><B>Intitulé du poste</B>- Employeur </h5>
+            <h6>Type de contrat </h6> <br>
+            <h6>Description du poste <button type="button" onclick="textecache('span_text3');">...</button> </h6>
+            <span id="span_text3" style="display: none";>Suite de la description trop longue</span>
+            <h6>Salaire</h6>
+        </div>
+    </div>
 
 
-	<footer>
+</nav>
 
-	</footer>
+<nav class ="CV">
+</nav>
+
+
+<footer>
+
+</footer>
 </body>
 </html>
-
