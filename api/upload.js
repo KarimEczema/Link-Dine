8aebd903f4e020565ad1dc6cb3c994d61be43442
KarimEczema <elhousnyadel@gmail.com>
2023-05-30 17:10:49 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -20,4 +20,37 @@ async function listFiles() {
     }
 }
 
+async function uploadFile(filePath, fileContent) {
+    try {
+        let { data, error } = await supabase
+            .storage
+            .from('Images')
+            .upload(filePath, fileContent);
+
+        if (error) throw error;
+
+        console.log("File uploaded:", data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function downloadFile(filePath) {
+    try {
+        const { data, error } = await supabase
+            .storage
+            .from('Images')
+            .download(filePath);
+
+        if (error) throw error;
+
+        // `data` contains the downloaded file as a Blob. 
+        // Here, we're just logging the Blob's size, but you could do other things with it.
+        console.log("Downloaded file size:", data.size);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
 
