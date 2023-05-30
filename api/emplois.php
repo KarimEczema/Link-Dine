d1f942d65edac1eb5f6a819e24b55770e57e8280
GauthierMallard <gauthier.mallard@edu.ece.fr>
2023-05-30 14:38:37 +0200

    pour pouvoir essayer

--- api/emplois.php
+++ api/emplois.php
@@ -3,9 +3,9 @@
 <head>
 <title>ECE-in</title>
 <meta charset="utf-8" />
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Chargement du JavaScript de Bootstrap -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+ Chargement du JavaScript de Bootstrap
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
 <script type="text/javascript">
 	$(document).ready(function() {
 		var $img = $('#carrousel img');
