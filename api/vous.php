47eb1946063dc80adcbfb941d0d0d1c9910de8d7
Nirziin <romain.barriere@edu.ece.fr>
2023-05-30 17:52:32 +0200

    +Footer -> chaque page
    Le footer bien placé est rajouté dans toutes les pages

--- api/vous.php
+++ api/vous.php
@@ -79,24 +79,24 @@ Description droite
 
 -->
 
-    <footer>
+<footer>
 		<div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6" style = "border : solid; color: black; padding:2px">
-                    <p>
-                        Ce site est une plate-forme en ligne permettant à des utilisateurs
-                        de se connecter avec son réseau dans un cadre professionnel, vous pouvez ainsi regarder des posts, des évènements ou
-                        des offres d'emploi.
-                        Chaque utilisateur peut aussi poster des évènements, des photos, des vidéos, son CV et le/s afficher, partager, etc...
-                    </p>
-                </div>
-                <div class="col-sm-6" style = "border : solid; color: black; padding:2px">
-                    <a mailto="adrienne.vidon@edu.ece.fr">Nous contacter</a>
-                    <!-- Insertion Maps? -->
-                </div>
-            </div>
+			<div class="row">
+				<div class="col-sm-6" style = "border : solid; color: black; padding:2px">
+					<p>
+					Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 d'utilisateurs dans plus de 0 pays et territoires du monde.
+					</p>
+				</div>
+				<div class="col-sm-6" style = "border : solid; color: black; padding:2px">
+					<p style="text-align : center;">Nous contacter</p>
+					
+					<a href="mailto:adrienne.vidon@edu.ece.fr"> Mail </a>
+					<br>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
 		</div>
-    </footer>
+	</footer>
 </body>
 
 </html>
