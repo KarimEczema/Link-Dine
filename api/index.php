f2d83c7d86f29e7d09f3f1b792534f6cbba01235
KarimEczema <elhousnyadel@gmail.com>
2023-05-30 12:35:28 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -14,7 +14,7 @@ try{
     // if form is submitted
     if($_POST){  
         // query to check if NomUtilisateur and Mdp are correct
-        $sql = "SELECT * FROM users WHERE username = :NomUtilisateur AND Mdp = :Mdp";
+        $sql = "SELECT * FROM users WHERE username = :NomUtilisateur AND password = :Mdp";
         $stmt = $conn->prepare($sql);
         
         // bind parameters and execute
