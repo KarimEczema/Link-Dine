b231f9f5b2543af83ff8e7594ae5d1c788135845
Gwendera <adrienne.vidon@edu.ece.fr>
2023-05-30 14:29:35 +0200

    mo

---
+++ api/vous.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ECE-in</title>
+<meta charset="utf-8" />
+</head>
+
+<body>
+<link href="css/vous.css" rel="stylesheet" type="text/css"/>
+
+<!--
+Bloc en haut
+
+Photo gauche
+
+Nom droite
+
+Description droite
+
+-->
+
+<!-- Création d'un conteneur à 2 colonnes -->
+<div class="container features">
+        <div class="row">
+
+            <!-- Colonne 1 -->
+        <div class="col-lg-4 col-md-4 col-sm-12">
+        <h3 class="feature-title">Lorem ipsum</h3>
+        <img src="Images/column1.jpg" class="img-fluid">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante 
+        luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
+        </p>
+        </div>
+            <!-- Colonne 2 -->
+        <div class="col-lg-4 col-md-4 col-sm-12">
+        <h3 class="feature-title">Lorem ipsum</h3>
+        <img src="Images/column2.jpg" class="img-fluid">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante
+        luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
+        </p>
+    </div>
+
+<!--
+    Formation middle
+
+    Bloc middle
+
+    Affichage des formations database 
+
+-->
+
+<!--
+    Ajouter une formation
+
+    Bloc sélection date début et fin
+
+    Nom de formation
+
+-->
+
+<!--
+    Projets
+
+    Affichage en carrousel des noms + Description des projets
+
+-->
+
+<!--
+    Ajouter un projet
+
+    Nom et description bdd
+
+-->
+
+<!--
+    Ajouter un CV (génération automatique) : boutton
+
+-->
+
+</body>
+
+</html>
