3fee1f41ad3dc7e3a327b732aaaad348691f1342
KarimEczema <elhousnyadel@gmail.com>
2023-05-30 14:57:02 +0200

    test

--- api/accueil.php
+++ api/accueil.php
@@ -3,7 +3,8 @@
 <head>
 <title>ECE-in</title>
 <meta charset="utf-8" />
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
 		var $img = $('#carrousel img');
