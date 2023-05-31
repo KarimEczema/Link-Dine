c81eae71a6e77d2b6da075eb6802b7d1e5cea4b2
Nirziin <romain.barriere@edu.ece.fr>
2023-05-31 16:34:23 +0200

    Oubli .css
    Rajout du link avec le css

--- api/profil.php
+++ api/profil.php
@@ -59,6 +59,7 @@ echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstr
     });
 </script>
 
+<link href="profil.css" rel="stylesheet" type="text/css"/>
 
 <body>
 	<header>
