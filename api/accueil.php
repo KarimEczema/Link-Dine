55dbf5f1561e5b0d7be2d2c7d9806928decd2950
Gwendera <adrienne.vidon@edu.ece.fr>
2023-05-31 10:20:56 +0200

    Update accueil.php

--- api/accueil.php
+++ api/accueil.php
@@ -101,8 +101,8 @@ echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstr
 				<div class="row">
 					 <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
 					 <div class="col-sm-2"><a href="Reseau.php" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div>
-					 <div class="col-sm-2"><a href="vous.php" style = "border : solid; color: black; padding:2px">Vous</a></div>
-					 <div class="col-sm-2"><a href="notifs.php" style = "border : solid; color: black; padding:2px">Notifications</a></div>
+					 <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
+					 <div class="col-sm-2"><a href="notifications" style = "border : solid; color: black; padding:2px">Notifications</a></div>
 					 <div class="col-sm-2"><a href="messages.php" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
 					 <div class="col-sm-2"><a href="emplois.php" style = "border : solid; color: black; padding:2px">Emplois</a></div>
 				</div>		
