5fe3e7ee0e364ccd9a0cd48630878630d1fe49d9
GauthierMallard <gauthier.mallard@edu.ece.fr>
2023-05-31 10:46:15 +0200

    modif notif finale

--- api/notifications.php
+++ api/notifications.php
@@ -7,74 +7,74 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <link href="css/notifications.css" rel="stylesheet" type="text/css"/>
+    <link href="css/index.css" rel="stylesheet" type="text/css"/>
 
 
 </head>
 
 <script type="text/javascript">
     $(document).ready(function() {
-		var $img = $('#carrousel img');
-		var max = $img.length;
-		var i = 0; // compteur
-		$img.css('margin-left','0').css('display', 'none'); //on cache les images
-		$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-		$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-		$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-		$img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-		//si on clique sur « next » ou « > »
-		$('.next').click(function () { // image suivante
-			 i+=4; // on incrémente le compteur
-			 if (i < max-4) {
-			 i = i+4;
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
-			 i = 0;
-			 }
-		 });
-		//si on clique sur « prev » ou « < »
-		 $('.prev').click(function () { // groupe des images précédentes
-			 i-=4; // on décrémente le compteur
-			 if (i >= 0) {
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-			 } else {
-			 i = 0;
-			 }
-		 });
-		function slideImg() {
-			 setTimeout(function() {
-			 $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','0.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','0.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','1s');
-			 if (i < max-4) {
-			i = i+4;
-			 } else {
-			i = 0;
-			 }
-			 $img.css('margin-left','0').css('display', 'none');
-			 $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','1.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','1.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','2s');
-			slideImg();
-			}, 4000);
-		}
-		slideImg();
-	});
+        var $img = $('#carrousel img');
+        var max = $img.length;
+        var i = 0; // compteur
+        $img.css('margin-left','0').css('display', 'none'); //on cache les images
+        $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+        $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+        $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+        $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
+        //si on clique sur « next » ou « > »
+        $('.next').click(function () { // image suivante
+            i+=4; // on incrémente le compteur
+            if (i < max-4) {
+                i = i+4;
+                $img.css('margin-left','0').css('display', 'none'); //on cache
+                $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+                $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+                $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+                $img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
+                i = 0;
+            }
+        });
+        //si on clique sur « prev » ou « < »
+        $('.prev').click(function () { // groupe des images précédentes
+            i-=4; // on décrémente le compteur
+            if (i >= 0) {
+                $img.css('margin-left','0').css('display', 'none'); //on cache
+                $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+                $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+                $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+                $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
+            } else {
+                i = 0;
+            }
+        });
+        function slideImg() {
+            setTimeout(function() {
+                $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
+                $img.eq(i + 1).css('margin-left','200px').css('display',
+                    'inline').css('transition-delay','0.5s');
+                $img.eq(i + 2).css('margin-left','400px').css('display',
+                    'inline').css('transition-delay','0.75s');
+                $img.eq(i + 3).css('margin-left','600px').css('display',
+                    'inline').css('transition-delay','1s');
+                if (i < max-4) {
+                    i = i+4;
+                } else {
+                    i = 0;
+                }
+                $img.css('margin-left','0').css('display', 'none');
+                $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
+                $img.eq(i + 1).css('margin-left','200px').css('display',
+                    'inline').css('transition-delay','1.5s');
+                $img.eq(i + 2).css('margin-left','400px').css('display',
+                    'inline').css('transition-delay','1.75s');
+                $img.eq(i + 3).css('margin-left','600px').css('display',
+                    'inline').css('transition-delay','2s');
+                slideImg();
+            }, 4000);
+        }
+        slideImg();
+    });
 </script>
 
 
@@ -107,52 +107,55 @@
     <div id = "Semaine">
         <h5> Evènement de la semaine</h5>
     </div>
-
+    <nav style="padding-bottom: 10%">
     <div id="carrousel">
-    			<ul style ="list-style-type : none;">
-    				<li><img src="images/Celeste.png" width="120" height="100"></li>
-    				<li><img src="images/Celeste_LVL8_FaceB.png" width="120" height="100"></li>
-    				<li><img src="images/CelesteScare.png" width="120" height="100"></li>
-    				<li><img src="images/CelesteTheo.png" width="120" height="100"></li>
-    				<li><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/CHIBIART%20FOR%20ADRIENNE.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvQ0hJQklBUlQgRk9SIEFEUklFTk5FLnBuZyIsImlhdCI6MTY4NTQ1MzYwOSwiZXhwIjoxNjg4MDQ1NjA5fQ.WPg1DleVb23PFe2EfTDyFgRNIIDuuhwx6LO7DDheIKU&t=2023-05-30T13%3A33%3A29.160Z" width="120" height="100"></li>
-    				<li><img src="images/HollowKnightWallPaper.jfif" width="120" height="100"></li>
-    				<li><img src="images/logECE.png" width="120" height="100"></li>
-    				<li><img src="images/StreetMordred.jpg" width="120" height="100"></li>
-    				<li><img src="book9.jpg" width="120" height="100"></li>
-    				<li><img src="book10.jpg" width="120" height="100"></li>
-    				<li><img src="book11.jpg" width="120" height="100"></li>
-    				<li><img src="book12.jpg" width="120" height="100"></li>
-    			</ul>
+        <ul style ="list-style-type : none;">
+            <li><img src="images/bird3.jpg" width="120" height="100" alt="img1"></li>
+            <li><img src="images/bird4.jpg" width="120" height="100" alt="img2"></li>
+            <li><img src="images/bird5.png" width="120" height="100" alt="img3"></li>
+            <li><img src="images/CelesteTheo.png" width="120" height="100" alt="img4"></li>
+            <li><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/CHIBIART%20FOR%20ADRIENNE.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvQ0hJQklBUlQgRk9SIEFEUklFTk5FLnBuZyIsImlhdCI6MTY4NTQ1MzYwOSwiZXhwIjoxNjg4MDQ1NjA5fQ.WPg1DleVb23PFe2EfTDyFgRNIIDuuhwx6LO7DDheIKU&t=2023-05-30T13%3A33%3A29.160Z" width="120" height="100" alt="imgsuppabase"></li>
+            <li><img src="images/HollowKnightWallPaper.jfif" width="120" height="100" alt="img5"></li>
+            <li><img src="images/logECE.png" width="120" height="100" alt="img6"></li>
+            <li><img src="images/StreetMordred.jpg" width="120" height="100" alt="img7"></li>
+            <li><img src="book9.jpg" width="120" height="100" alt="img8"></li>
+            <li><img src="book10.jpg" width="120" height="100" alt="img9"></li>
+            <li><img src="book11.jpg" width="120" height="100" alt="img10"></li>
+            <li><img src="book12.jpg" width="120" height="100" alt="img11"></li>
+        </ul>
+    </div>
+    </nav>
+    <nav style="padding-top: 3%; padding-bottom: 6%">
+        <div id="buttons" >
+            <input type="button" value="<" class="prev">
+            <input type="button" value=">" class="next">
+        </div>
+    </nav>
 
-    		</div>
-    		<div id="buttons">
-    			<input type="button" value="<" class="prev">
-    			<input type="button" value=">" class="next">
-    		</div>
 </nav>
 
 <nav class="section">
     <div id = "Amis">
-            <h5> Que font mes amis ?</h5>
+        <h5> Que font mes amis</h5>
     </div>
 
     <div class="scroll-container">
-            <div class="scroll-page" id="notif-1">
-                <h5>Nom de l'amis </h5>
-                <h6>à : Action </h6>
-                <h6>30/05/2023</h6>
-            </div>
-            <div class="scroll-page" id="notif-2">
-                <h5>Nom de l'amis  </h5>
-                <h6>à : Action </h6>
-                <h6>30/05/2023</h6>
-            </div>
-            <div class="scroll-page" id="notif-3">
-                <h5>Nom de l'amis  </h5>
-                <h6>à : Action </h6>
-                <h6>29/05/2023</h6>
-            </div>
+        <div class="scroll-page" id="notif-1">
+            <h5>Nom de l'amis </h5>
+            <h6>à : Action </h6>
+            <h6>30/05/2023</h6>
         </div>
+        <div class="scroll-page" id="notif-2">
+            <h5>Nom de l'amis  </h5>
+            <h6>à : Action </h6>
+            <h6>30/05/2023</h6>
+        </div>
+        <div class="scroll-page" id="notif-3">
+            <h5>Nom de l'amis  </h5>
+            <h6>à : Action </h6>
+            <h6>29/05/2023</h6>
+        </div>
+    </div>
 
 </nav>
 
