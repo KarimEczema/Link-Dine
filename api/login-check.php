6e80d0f92cfaa072d386500bde601c2c4910fedd
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 12:01:42 +0200

    TEST

--- api/login-check.php
+++ api/login-check.php
@@ -1,11 +1,36 @@
 <?php
-session_start();
+// Include the JWT library
+require 'path/to/firebase/php-jwt/JWT.php';
 
-if (!isset($_COOKIE['username'])) {
-    header('Location: index');
-    exit();
-} 
+use \Firebase\JWT\JWT;
+
+// Your secret key
+$secretKey = '123';
+
+// Check if the JWT exists in the cookie
+if (isset($_COOKIE['jwt'])) {
+    // Get the JWT from the cookie
+    $jwt = $_COOKIE['jwt'];
     
-
-
+    try {
+        // Decode the JWT
+        $decoded = JWT::decode($jwt, $secretKey, array('HS256'));
+        
+        // Get the username from the decoded payload
+        $username = $decoded->username;
+        
+        // Continue processing or redirect to authenticated page
+        echo "Logged in as: " . $username;
+    } catch (Exception $e) {
+        // JWT validation failed
+        // Redirect to login page or show error message
+        header('Location: index.php');
+        exit;
+    }
+} else {
+    // JWT is not set, user is not logged in
+    // Redirect to login page or show error message
+    header('Location: index.php');
+    exit;
+}
 ?>
