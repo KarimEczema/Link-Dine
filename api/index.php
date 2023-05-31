e01cec8d7953bcbe1f27355f463d290300a45ba7
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 11:47:07 +0200

    led

--- api/index.php
+++ api/index.php
@@ -28,9 +28,9 @@ try{
             // assuming that your password field is 'Mdp'
             if($_POST['Mdp'] === $user['password']){
                 session_start();
-                $_SESSION['username'] = $user['username'];
+                //$_SESSION['username'] = $user['username'];
                 setcookie('username', $username, time()+3600); 
-                echo $_SESSION['username'];
+                //echo $_SESSION['username'];
                 echo '<meta http-equiv="refresh" content="0; url= accueil" />';
                 exit;
             }
--- api/login-check.php
+++ api/login-check.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 
-if (isset($_COOKIE['username'])) {
-    $username = $_COOKIE['username'];
-    //echo $_SESSION['username'];
-    //header('Location: index');
+if (!isset($_COOKIE['username'])) {
+    header('Location: index');
     exit();
-} else {
-    // Redirect to login page
-}
+} 
     
 
 
