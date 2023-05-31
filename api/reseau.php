36f5689ff5d53c318faaad535bfd43a327213e30
Nirziin <romain.barriere@edu.ece.fr>
2023-05-31 11:12:48 +0200

    Last changes

--- api/reseau.php
+++ api/reseau.php
@@ -1,74 +1,87 @@
-<head>
-	<title>ECE-in</title>
-	<meta charset="utf-8" />
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Chargement du JavaScript de Bootstrap -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function() {
-			var $img = $('#carrousel img');
-			var max = $img.length;
-			var i = 0; // compteur
-			$img.css('margin-left','0').css('display', 'none'); //on cache les images
-			$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			$img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-			//si on clique sur « next » ou « > »
-			$('.next').click(function () { // image suivante
-				i+=4; // on incrémente le compteur
-				if (i < max-4) {
-				i = i+4;
-				$img.css('margin-left','0').css('display', 'none'); //on cache
-				$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-				$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-				$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-				$img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
-				i = 0;
-				}
-			});
-			//si on clique sur « prev » ou « < »
-			$('.prev').click(function () { // groupe des images précédentes
-				i-=4; // on décrémente le compteur
-				if (i >= 0) {
-				$img.css('margin-left','0').css('display', 'none'); //on cache
-				$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-				$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-				$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-				$img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-				} else {
-				i = 0;
-				}
-			});
-			function slideImg() {
-				setTimeout(function() {
-				$img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
-				$img.eq(i + 1).css('margin-left','200px').css('display',
-				'inline').css('transition-delay','0.5s');
-				$img.eq(i + 2).css('margin-left','400px').css('display',
-				'inline').css('transition-delay','0.75s');
-				$img.eq(i + 3).css('margin-left','600px').css('display',
-				'inline').css('transition-delay','1s');
-				if (i < max-4) {
-				i = i+4;
-				} else {
-				i = 0;
-				}
-				$img.css('margin-left','0').css('display', 'none');
-				$img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
-				$img.eq(i + 1).css('margin-left','200px').css('display',
-				'inline').css('transition-delay','1.5s');
-				$img.eq(i + 2).css('margin-left','400px').css('display',
-				'inline').css('transition-delay','1.75s');
-				$img.eq(i + 3).css('margin-left','600px').css('display',
-				'inline').css('transition-delay','2s');
-				slideImg();
-				}, 4000);
-			}
-			slideImg();
-		});
-	</script>
-</head>
+<?php
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
+
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+
+
+echo '<html>';
+echo '<head>';
+echo '<title>ECE-in</title>';
+
+// Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
+echo '<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">';
+echo '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>';
+echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>';
+
+?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var $img = $('#carrousel img');
+        var max = $img.length;
+        var i = 0; // compteur
+        $img.css('margin-left','0').css('display', 'none'); //on cache les images
+        $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+        $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+        $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+        $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
+        //si on clique sur « next » ou « > »
+        $('.next').click(function () { // image suivante
+            i+=4; // on incrémente le compteur
+            if (i < max-4) {
+            i = i+4;
+            $img.css('margin-left','0').css('display', 'none'); //on cache
+            $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+            $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+            $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+            $img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
+            i = 0;
+            }
+        });
+        //si on clique sur « prev » ou « < »
+        $('.prev').click(function () { // groupe des images précédentes
+            i-=4; // on décrémente le compteur
+            if (i >= 0) {
+            $img.css('margin-left','0').css('display', 'none'); //on cache
+            $img.eq(i).css('display', 'inline'); //on affiche l'image courante
+            $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
+            $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
+            $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
+            } else {
+            i = 0;
+            }
+        });
+        function slideImg() {
+            setTimeout(function() {
+            $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
+            $img.eq(i + 1).css('margin-left','200px').css('display',
+            'inline').css('transition-delay','0.5s');
+            $img.eq(i + 2).css('margin-left','400px').css('display',
+            'inline').css('transition-delay','0.75s');
+            $img.eq(i + 3).css('margin-left','600px').css('display',
+            'inline').css('transition-delay','1s');
+            if (i < max-4) {
+            i = i+4;
+            } else {
+            i = 0;
+            }
+            $img.css('margin-left','0').css('display', 'none');
+            $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
+            $img.eq(i + 1).css('margin-left','200px').css('display',
+            'inline').css('transition-delay','1.5s');
+            $img.eq(i + 2).css('margin-left','400px').css('display',
+            'inline').css('transition-delay','1.75s');
+            $img.eq(i + 3).css('margin-left','600px').css('display',
+            'inline').css('transition-delay','2s');
+            slideImg();
+            }, 4000);
+        }
+        slideImg();
+    });
+</script>
 
 <link href="css/accueil.css" rel="stylesheet" type="text/css"/>
 <body>
@@ -114,7 +127,8 @@
 			<li><img src="book11.jpg" width="120" height="100"></li>
 			<li><img src="book12.jpg" width="120" height="100"></li>
 		</ul>
-		<!-- Recuperer les noms, les liens et statut + description.-->
+
+		<!-- Recuperer les noms, les liens et statut + description en php.-->
 
 	</div>
 	<div id="buttons">
--- api/vous.php
+++ api/vous.php
@@ -1,29 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>ECE-in</title>
-<meta charset="utf-8" />
-</head>
+<?php
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
 
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+
+
+echo '<html>';
+echo '<head>';
+echo '<title>ECE-in</title>';
+
+// Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
+echo '<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">';
+echo '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>';
+echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>';
+
+?>
 <body>
-<link href="css/vous.css" rel="stylesheet" type="text/css"/>
-
-
-	
-<nav class = "navigation">
-    <div class="container-fluid">
-            <div class="row">
-                    <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
-                    <div class="col-sm-2"><a href="reseau" style = "border : solid; color: black; padding:2px">Réseau</a></div>
-                    <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
-                    <div class="col-sm-2"><a href="notifs" style = "border : solid; color: black; padding:2px">Notifications</a></div>
-                    <div class="col-sm-2"><a href="messages" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
-                    <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div>
-            </div>
-    </div>
-</nav>
+    <nav class = "navigation">
+        <div class="container-fluid">
+                <div class="row">
+                        <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
+                        <div class="col-sm-2"><a href="reseau" style = "border : solid; color: black; padding:2px">Réseau</a></div>
+                        <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
+                        <div class="col-sm-2"><a href="notifs" style = "border : solid; color: black; padding:2px">Notifications</a></div>
+                        <div class="col-sm-2"><a href="messages" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
+                        <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div>
+                </div>
+        </div>
+    </nav>
 <!--
-    
+
 Photo gauche
 
 Nom droite
