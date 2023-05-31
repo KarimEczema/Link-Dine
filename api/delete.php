d781755f6fe9322f0a9b45965ca06883d469aca8
KarimEczema <elhousnyadel@gmail.com>
2023-06-01 00:03:38 +0200

    Update delete.php

--- api/delete.php
+++ api/delete.php
@@ -1,4 +1,15 @@
 <?php
+
+
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
+
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+
+
 try{
     // create a PostgreSQL database connection
     $conn = new PDO($dsn);
