31038539c9a0eda2228d05c140c28dadace1a3c5
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 21:50:32 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -156,7 +156,7 @@ try{
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="js/delete.js"></script>
-    <script type="text/javascript">
+    <script >
         const supabaseUrl = 'https://bmqgiyygwjnnfyrtjkno.supabase.co';
         const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcWdpeXlnd2pubmZ5cnRqa25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNzM1NzcsImV4cCI6MjAwMDk0OTU3N30.sQgvRElC6O5e4uE8OVZqLXBiQYQa83mSkTy4s4L0aDw'
 
