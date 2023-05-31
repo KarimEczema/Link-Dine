b7bc544c6163fc5ccdd55dbcf4c6c975bfcb82bd
Gwendera <adrienne.vidon@edu.ece.fr>
2023-05-31 16:36:22 +0200

    bdd

--- api/login-check.php
+++ api/login-check.php
@@ -2,6 +2,16 @@
 // Include the JWT library
 require __DIR__ . '/vendor/autoload.php';
 
+
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
+
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+
+
 use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key;
 
