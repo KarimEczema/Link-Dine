1a79e4e3a641999171645f33c4b3abb04430fb9a
Gwendera <adrienne.vidon@edu.ece.fr>
2023-05-31 11:39:19 +0200

    Pb main

--- api/emplois.php
+++ api/emplois.php
@@ -1,95 +1,96 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html> 
+<html> 
 
-<head>
-    <meta charset="utf-8">
-    <title>Page Emplois</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <link href="css/emploi.css" rel="stylesheet" type="text/css"/>
+<head> 
+    <meta charset="utf-8"> 
+    <title>Page Emplois</title> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> 
+    <link href="css/emploi.css" rel="stylesheet" type="text/css"/> 
 
 
-</head>
-
-<script type="text/javascript">
-    function textecache(ntexte){
-        var span =document.getElementById(ntexte);
-        if(span.style.display === "none")
-        {
-            span.style.display="inline";
-        }
-        else
-        {
-            span.style.display="none";
-        }
+</head> 
+ 
+<script type="text/javascript"> 
+    function textecache(ntexte){ 
+        var span =document.getElementById(ntexte); 
+        if(span.style.display === "none") 
+        { 
+            span.style.display="inline"; 
+        } 
+        else 
+        { 
+            span.style.display="none"; 
+        } 
     }
-</script>
+</script> 
 
 
-<body>
-<header>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
-            <div class="col-sm" ><img src="LogECE.png" width="121" height="49.5"></div>
-        </div>
-    </div>
-</header>
+<body> 
+<header> 
+    <div class="container-fluid"> 
+        <div class="row"> 
+            <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div> 
+            <div class="col-sm" ><img src="LogECE.png" width="121" height="49.5"></div> 
+        </div> 
+    </div> 
+</header> 
 
-<nav class = "navigation">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
-            <div class="col-sm-2"><a href="Reseau" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div>
-            <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
-            <div class="col-sm-2"><a href="notifications" style = "border : solid; color: black; padding:2px">Notifications</a></div>
-            <div class="col-sm-2"><a href="messages" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
-            <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div>
-        </div>
-    </div>
-</nav>
-
-
-
-<nav class = "section">
-    <div id = "Emplois">
-        <h5> Offres d'emploi</h5>
-    </div>
-
-    <div class="scroll-container">
-        <div class="scroll-page" id="formation-1">
-            <h5><B>Intitulé du poste</B>- Employeur </h5>
-            <h6>Type de contrat </h6> <br>
-            <h6>Description du poste <button type="button" onclick="textecache('span_text1');">...</button> </h6>
-            <span id="span_text1" style="display: none";>Suite de la description trop longue</span>
-            <h6>Salaire</h6>
-        </div>
-        <div class="scroll-page" id="formation-2">
-            <h5><B>Intitulé du poste</B>- Employeur </h5>
-            <h6>Type de contrat </h6> <br>
-            <h6>Description du poste <button type="button" onclick="textecache('span_text2');">...</button> </h6>
-            <span id="span_text2" style="display: none";>Suite de la description trop longue</span>
-            <h6>Salaire</h6>
-        </div>
-        <div class="scroll-page" id="formation-3">
-            <h5><B>Intitulé du poste</B>- Employeur </h5>
-            <h6>Type de contrat </h6> <br>
-            <h6>Description du poste <button type="button" onclick="textecache('span_text3');">...</button> </h6>
-            <span id="span_text3" style="display: none";>Suite de la description trop longue</span>
-            <h6>Salaire</h6>
-        </div>
-    </div>
+<nav class = "navigation"> 
+    <div class="container-fluid"> 
+        <div class="row"> 
+            <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div> 
+            <div class="col-sm-2"><a href="Reseau" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div> 
+            <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div> 
+            <div class="col-sm-2"><a href="notifications" style = "border : solid; color: black; padding:2px">Notifications</a></div> 
+            <div class="col-sm-2"><a href="messages" style = "border : solid; color: black; padding:2px">Messagerie</a></div> 
+            <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div> 
+        </div> 
+    </div> 
+</nav> 
 
 
-</nav>
 
-<nav class ="CV">
-</nav>
+<nav class = "section"> 
+    <div id = "Emplois"> 
+        <h5> Offres d'emploi</h5> 
+    </div> 
+ 
+    <div class="scroll-container"> 
+        <div class="scroll-page" id="formation-1"> 
+            <h5><B>Intitulé du poste</B>- Employeur </h5> 
+            <h6>Type de contrat </h6> <br> 
+            <h6>Description du poste <button type="button" onclick="textecache('span_text1');">...</button> </h6> 
+            <span id="span_text1" style="display: none";>Suite de la description trop longue</span> 
+            <h6>Salaire</h6> 
+        </div> 
+        <div class="scroll-page" id="formation-2"> 
+            <h5><B>Intitulé du poste</B>- Employeur </h5> 
+            <h6>Type de contrat </h6> <br> 
+            <h6>Description du poste <button type="button" onclick="textecache('span_text2');">...</button> </h6> 
+            <span id="span_text2" style="display: none";>Suite de la description trop longue</span> 
+            <h6>Salaire</h6> 
+        </div> 
+        <div class="scroll-page" id="formation-3"> 
+            <h5><B>Intitulé du poste</B>- Employeur </h5> 
+            <h6>Type de contrat </h6> <br> 
+            <h6>Description du poste <button type="button" onclick="textecache('span_text3');">...</button> </h6> 
+            <span id="span_text3" style="display: none";>Suite de la description trop longue</span> 
+            <h6>Salaire</h6> 
+        </div> 
+    </div> 
 
 
-<footer>
+</nav> 
 
-</footer>
-</body>
-</html>
+<nav class ="CV"> 
+</nav> 
+
+
+<footer> 
+
+</footer> 
+</body> 
+</html> 
+ 
