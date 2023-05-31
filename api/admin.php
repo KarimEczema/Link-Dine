6a33935136bdaa90e7e716ce15ff7825eff538c8
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 21:53:36 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -155,8 +155,8 @@ try{
     <button id="sendButton" style="padding:5%">Supprimer l'utilisateur</button>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="js/delete.js"></script>
-    <script >
+	
+    <script>
         const supabaseUrl = 'https://bmqgiyygwjnnfyrtjkno.supabase.co';
         const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcWdpeXlnd2pubmZ5cnRqa25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNzM1NzcsImV4cCI6MjAwMDk0OTU3N30.sQgvRElC6O5e4uE8OVZqLXBiQYQa83mSkTy4s4L0aDw'
 
