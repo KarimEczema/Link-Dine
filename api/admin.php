467e931920db7c6d3e460cd5b125f095a93705e7
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 23:04:13 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -11,9 +11,9 @@ include 'admin-check.php';
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1.11.6/dist/umd/supabase.min.js"></script>
 
 
+<script src="js/delete.js" ></script>
 <script type="text/javascript">
 	$(document).ready(function() {
 		var $img = $('#carrousel img');
