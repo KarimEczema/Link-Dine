c36a7b6d02c45288520ae2f3d34db4396133cc0a
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 21:19:42 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -2,6 +2,10 @@
 
 include 'login-check.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers');
+
 echo '<html>';
 echo '<head>';
 echo '<title>Admin</title>';
