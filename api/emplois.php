71b94aaba9bf5a0aabb09380563df7b20fa27403
Gwendera <adrienne.vidon@edu.ece.fr>
2023-05-31 11:30:07 +0200

    Revert "Merge branch 'main' into Adrienne"
    This reverts commit 9f7b46fef613c66db710f26acfce366f6a7ce4cc, reversing
    changes made to 2e6ecc8495936f1fd1fbf9e333cbf49b1b972aa5.

--- api/emplois.php
+++ api/emplois.php
@@ -28,33 +28,11 @@
 
 
 <body>
-	<header>
-		<div class="container-fluid">
-			<div class="row">
-				 <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
-				 <div class="col-sm" ><img src="images/LogECE.png" width="121" height="49.5"></div>
-			</div>
-		</div>
-	</header>
-
-	<nav class = "navigation">
-		<div class="container-fluid">
-				<div class="row">
-					 <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
-					 <div class="col-sm-2"><a href="reseau" style = "border : solid; color: black; padding:2px">Réseau</a></div>
-					 <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
-					 <div class="col-sm-2"><a href="notifs" style = "border : solid; color: black; padding:2px">Notifications</a></div>
-					 <div class="col-sm-2"><a href="messages" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
-					 <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div>
-				</div>
-		</div>
-	</nav>
-
-
-
-	<nav class = "section">
-		<div id = "Emplois">
-			<h5 style = "text-align : center; color:red"> Offres d'emploi</h5>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
+            <div class="col-sm" ><img src="LogECE.png" width="121" height="49.5"></div>
         </div>
     </div>
 </header>
@@ -104,23 +82,14 @@
     </div>
 
 
-	<footer>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm-6" style = "border : solid; color: black; padding:2px">
-					<p>
-					Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 d'utilisateurs dans plus de 0 pays et territoires du monde.
-					</p>
-				</div>
-				<div class="col-sm-6" style = "border : solid; color: black; padding:2px">
-					<p style="text-align : center;">Nous contacter</p>
-					
-					<a href="mailto:adrienne.vidon@edu.ece.fr"> Mail </a>
-					<br>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-				</div>
-			</div>
-		</div>
-	</footer>
+</nav>
+
+<nav class ="CV">
+</nav>
+
+
+<footer>
+
+</footer>
 </body>
 </html>
