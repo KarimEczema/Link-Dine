4baf2eea48e9d0cddd20a1baea693a3e7e01405c
KarimEczema <elhousnyadel@gmail.com>
2023-05-31 22:18:45 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -9,9 +9,6 @@ include 'login-check.php';
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-<script src="js/delete.js"></script>
-
-
 
 <script type="text/javascript">
 	$(document).ready(function() {
