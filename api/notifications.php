93896ac1fff6833fa101c8f8fc2b5024aab3232c
Nirziin <romain.barriere@edu.ece.fr>
2023-06-01 17:02:12 +0200

    Patch notifs v1

--- api/notifications.php
+++ api/notifications.php
@@ -1,52 +1,78 @@
 <?php
-    include 'login-check.php';
+
+include 'login-check.php';
 
 echo '<html>';
 echo '<head>';
-echo '<title>Admin</title>';
+echo '<title>Accueil</title>';
+
+// Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
+echo '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>';
+echo "Logged in as: " . $iduser;
 echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'; 
-echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> '; 
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>'; 
-echo '<link rel="stylesheet" type="text/css" href="css/notifications.css">'; 
+echo '<link rel="stylesheet" type="text/css" href="css/accueil.css">'; 
+echo '<link rel="stylesheet" type="text/css" href="css/global.css">'; 
 echo '<body>';
 
 include 'navbar.php';
+include 'caroussel.php';
 ?>
-
-
 <body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
+                <div class="col-sm" ><img src="LogECE.png" width="121" height="49.5"></div>
+            </div>
+        </div>
+    </header>
+
+    <nav class = "navigation">
+		<ul id="liste1">
+
+
+
     <nav class = "section">
         <div id = "Semaine">
-            <h5 style = "text-align : center; color:red; border: 3px solid black; border-radius: 5%; padding : 3px;"> Evènement de la semaine</h5>
+            <h5> Evènement de la semaine</h5>
+        </div>
+        <nav style="padding-bottom: 10%">
+        <?php include 'carrousel.php' ?>
+        </nav>
+        <nav style="padding-top: 3%; padding-bottom: 6%">
+            <div id="buttons" >
+                <input type="button" value="<" class="prev">
+                <input type="button" value=">" class="next">
+            </div>
+        </nav>
+
+    </nav>
+
+    <nav class="section">
+        <div id = "Amis">
+            <h5> Que font mes amis</h5>
         </div>
 
         <div class="scroll-container">
-            <div class="scroll-page" id="formation-1">
-                <h5><B>Intitulé du poste</B>- Employeur </h5>
-                <h6>Type de contrat </h6> <br>
-                <h6>Description du poste <button type="button" onclick="textecache('span_text1');">...</button> </h6>
-                <span id="span_text1" style="display: none";>Suite de la description trop longue</span>
-                <h6>Salaire</h6>
+            <div class="scroll-page" id="notif-1">
+                <h5>Nom de l'amis </h5>
+                <h6>à : Action </h6>
+                <h6>30/05/2023</h6>
             </div>
-            <div class="scroll-page" id="formation-2">
-                <h5><B>Intitulé du poste</B>- Employeur </h5>
-                <h6>Type de contrat </h6> <br>
-                <h6>Description du poste <button type="button" onclick="textecache('span_text2');">...</button> </h6>
-                <span id="span_text2" style="display: none";>Suite de la description trop longue</span>
-                <h6>Salaire</h6>
+            <div class="scroll-page" id="notif-2">
+                <h5>Nom de l'amis  </h5>
+                <h6>à : Action </h6>
+                <h6>30/05/2023</h6>
             </div>
-            <div class="scroll-page" id="formation-3">
-                <h5><B>Intitulé du poste</B>- Employeur </h5>
-                <h6>Type de contrat </h6> <br>
-                <h6>Description du poste <button type="button" onclick="textecache('span_text3');">...</button> </h6>
-                <span id="span_text3" style="display: none";>Suite de la description trop longue</span>
-                <h6>Salaire</h6>
+            <div class="scroll-page" id="notif-3">
+                <h5>Nom de l'amis  </h5>
+                <h6>à : Action </h6>
+                <h6>29/05/2023</h6>
             </div>
         </div>
 
-        <div id = "Emplois">
-                <h5 style = "text-align : center; color:red; border: 3px solid black; border-radius: 5%; padding : 3px;"> Evènement de la semaine</h5>
-        </div>
     </nav>
-    <?php include 'foot.php';?>
+    <?php include 'foot.php' ?>
+    
 </body>
