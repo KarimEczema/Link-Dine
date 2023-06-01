e298fb60e7132ca321508d40ad5190c378159546
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-01 09:40:54 +0200

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -16,103 +16,14 @@ include 'navbar.php';
 <script>                import { createClient } from '@supabase/supabase-js';
 </script>
 
-
-
-
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-
-<script type="text/javascript">
-	$(document).ready(function() {
-		var $img = $('#carrousel img');
-		var max = $img.length;
-		var i = 0; // compteur
-		$img.css('margin-left','0').css('display', 'none'); //on cache les images
-		$img.eq(i).css('display', 'inline'); //on affiche l'image courante
-		$img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-		$img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-		$img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-		//si on clique sur « next » ou « > »
-		$('.next').click(function () { // image suivante
-			 i+=4; // on incrémente le compteur
-			 if (i < max-4) {
-			 i = i+4;
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline'); } else {
-			 i = 0;
-			 }
-		 });
-		//si on clique sur « prev » ou « < »
-		 $('.prev').click(function () { // groupe des images précédentes
-			 i-=4; // on décrémente le compteur
-			 if (i >= 0) {
-			 $img.css('margin-left','0').css('display', 'none'); //on cache
-			 $img.eq(i).css('display', 'inline'); //on affiche l'image courante
-			 $img.eq(i+1).css('margin-left','200px').css('display', 'inline');
-			 $img.eq(i+2).css('margin-left','400px').css('display', 'inline');
-			 $img.eq(i+3).css('margin-left','600px').css('display', 'inline');
-			 } else {
-			 i = 0;
-			 }
-		 });
-		function slideImg() {
-			 setTimeout(function() {
-			 $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','0.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','0.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','1s');
-			 if (i < max-4) {
-			i = i+4;
-			 } else {
-			i = 0;
-			 }
-			 $img.css('margin-left','0').css('display', 'none');
-			 $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
-			 $img.eq(i + 1).css('margin-left','200px').css('display',
-			 'inline').css('transition-delay','1.5s');
-			 $img.eq(i + 2).css('margin-left','400px').css('display',
-			 'inline').css('transition-delay','1.75s');
-			 $img.eq(i + 3).css('margin-left','600px').css('display',
-			 'inline').css('transition-delay','2s');
-			slideImg();
-			}, 4000);
-		}
-		slideImg();
-	});
-</script>
 
 <link href="css/admin.css" rel="stylesheet" type="text/css"/>
 
 </head>
 <body>
-	<header>
-		<div class="container-fluid">
-			<div class="row">
-				 <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
-				 <div class="col-sm" ><img src="" width="121" height="49.5"></div>
-			</div>		
-		</div>
-	</header>
-	
-	<nav class = "navigation">
-		<div class="container-fluid">
-				<div class="row">
-					 <div class="col-sm-2"><a href="accueil" style = "border : solid; color: black; padding:2px">Accueil</a></div>
-					 <div class="col-sm-2"><a href="reseau" style = "border : solid; color: black; padding:2px">Mon Réseau</a></div>
-					 <div class="col-sm-2"><a href="vous" style = "border : solid; color: black; padding:2px">Vous</a></div>
-					 <div class="col-sm-2"><a href="notifications" style = "border : solid; color: black; padding:2px">Notifications</a></div>
-					 <div class="col-sm-2"><a href="chat" style = "border : solid; color: black; padding:2px">Messagerie</a></div>
-					 <div class="col-sm-2"><a href="emplois" style = "border : solid; color: black; padding:2px">Emplois</a></div>
-				</div>		
-		</div>
-	</nav>
 	
 	<nav class = "Ajout-compte">
 
