de27c32f5e0a677ab730c2606182245c6ac2c891
Nirziin <romain.barriere@edu.ece.fr>
2023-06-01 16:45:38 +0200

    Suppression slideImg + update css
    Update css de notifs
    
    Suppression du slide des images des carrousels.

--- api/caroussel.php
+++ api/caroussel.php
@@ -34,31 +34,5 @@
 			 i = 0;
 			 }
 		 });
-		function slideImg() {
-			 setTimeout(function() {
-			 $img.eq(i).css('display', 'inline').css('transition-delay','0.25s');
-			 $img.eq(i + 1).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','0.5s');
-			 $img.eq(i + 2).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','0.75s');
-			 $img.eq(i + 3).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','1s');
-			 if (i < max-4) {
-			i = i+4;
-			 } else {
-			i = 0;
-			 }
-			 $img.css('margin-left','0').css('display', 'none');
-			 $img.eq(i).css('display', 'inline').css('transition-delay','1.25s');
-			 $img.eq(i + 1).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','1.5s');
-			 $img.eq(i + 2).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','1.75s');
-			 $img.eq(i + 3).css('margin-left','0').css('display',
-			 'inline').css('transition-delay','2s');
-			slideImg();
-			}, 4000);
-		}
-		slideImg();
 	});
 </script>
