bf8821667ab775a5ba0e4846182ee3c6a643077b
Nirziin <romain.barriere@edu.ece.fr>
2023-06-01 20:27:02 +0200

    Correction de fautes + update carrousel

--- api/notifications.php
+++ api/notifications.php
@@ -18,30 +18,30 @@ include 'navbar.php';
 
 <body>
     <nav class = "section">
-        <div id = "Semaine" style = "color:red;">
-            <h5> Evènement de la semaine</h5>
-        </div>
-        <div id="carrousel">
-            <ul id = "listc" style ="list-style-type : none;">
-                <li><img src="images/Celeste.png" width="120" height="100"></li>
-                <li><img src="images/Celeste_LVL8_FaceB.png" width="120" height="100"></li>	
-                <li><img src="images/CelesteScare.png" width="120" height="100"></li>
-                <li><img src="images/CelesteTheo.png" width="120" height="100"></li>
-                <li><img src="chibiartforadrienne" width="120" height="100"></li>
-                <li><img src="images/HollowKnightWallPaper.jfif" width="120" height="100"></li>
-                <li><img src="images/logECE.png" width="120" height="100"></li>
-                <li><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/StreetMordred.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvU3RyZWV0TW9yZHJlZC5qcGciLCJpYXQiOjE2ODU1NDkyNTYsImV4cCI6MTY4ODE0MTI1Nn0.FOqtr6jvNjSmCcK9k_CeAyBUuo3k_VSmS0VVub_mago&t=2023-05-31T16%3A07%3A38.151Z" width="120" height="100"></li>
-                <li><img src="book9.jpg" width="120" height="100"></li>
-                <li><img src="book10.jpg" width="120" height="100"></li>
-                <li><img src="book11.jpg" width="120" height="100"></li>
-                <li><img src="book12.jpg" width="120" height="100"></li>
-            </ul>
-        </div>
-        <div id="buttons" >
-            <input type="button" value="<" class="prev">
-            <input type="button" value=">" class="next">
-        </div>
-    </nav>
+		<div id = "Event">
+			<h5 style = "text-align : center; color:red"> Evènements</h5>
+		</div>
+		<div id="carrousel">
+			<ul id = "listc" style ="list-style-type : none;">
+				<li><img src="images/Celeste.png" width="120" height="100"></li>
+				<li><img src="images/Celeste_LVL8_FaceB.png" width="120" height="100"></li>	
+				<li><img src="images/CelesteScare.png" width="120" height="100"></li>
+				<li><img src="images/CelesteTheo.png" width="120" height="100"></li>
+				<li><img src="chibiartforadrienne" width="120" height="100"></li>
+				<li><img src="images/HollowKnightWallPaper.jfif" width="120" height="100"></li>
+				<li><img src="images/logECE.png" width="120" height="100"></li>
+				<li><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/StreetMordred.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvU3RyZWV0TW9yZHJlZC5qcGciLCJpYXQiOjE2ODU1NDkyNTYsImV4cCI6MTY4ODE0MTI1Nn0.FOqtr6jvNjSmCcK9k_CeAyBUuo3k_VSmS0VVub_mago&t=2023-05-31T16%3A07%3A38.151Z" width="120" height="100"></li>
+				<li><img src="book9.jpg" width="120" height="100"></li>
+				<li><img src="book10.jpg" width="120" height="100"></li>
+				<li><img src="book11.jpg" width="120" height="100"></li>
+				<li><img src="book12.jpg" width="120" height="100"></li>
+			</ul>
+		</div>
+		<div id="buttons">
+			<input type="button" value="<" class="prev">
+			<input type="button" value=">" class="next">
+		</div>
+	</nav>
 
     <nav class="section" style = "color : black;">
         <div id = "Amis">
@@ -67,7 +67,7 @@ include 'navbar.php';
         </div>
         
     </nav>
-    
+
     <?php include 'foot.php' ?>
     
 </body>
