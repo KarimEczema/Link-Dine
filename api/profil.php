0afe95e6ef9c95aeaca8d409ebccb430b2e09ed6
GauthierMallard <gauthier.mallard@edu.ece.fr>
2023-06-02 14:03:48 +0200

    premier test de profil

--- api/profil.php
+++ api/profil.php
@@ -24,40 +24,79 @@ echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstr
 ?>
 
 <link href="css/profil.css" rel="stylesheet" type="text/css"/>
+<!--
+============================================
+        Profil de l'ami séléctionné
+============================================
+-->
+
+<!-- recupération en php des informations de la BDD -->
+<?php
+
+$sql = "SELECT * FROM user WHERE iduser= (SELECT amiexplore FROM user WHERE iduser= $iduser)";
+try{
+    // Création du contact avec la BDD
+    $conn = new PDO($dsn);
+    $stmt = $conn->query($sql);
+
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
+
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+
 
 <body>
 	<nav class = "profil" style = "border : solid; color: black; padding:7px">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-3">
-					<img src="image" width="200" height="200">
-				</div>
-				<div class="col-sm-9">
-					<p><b>  [Nom]</b> [Statut] </p>
-					<h6>Description : </h6> 
-					<p>Ceci est ma description</p>  
-				</div>
-			</div>
+            <table>
+                <tbody>
+                <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img src="image" alt="Cet utilisateur n'a pas de photo de profil" width="200" height="200">
+                        </div>
+                        <div class="col-sm-9">
+                            <p><b>  <?php echo htmlspecialchars($row['nom']); ?></b> <?php echo htmlspecialchars($row['statut']); ?> </p>
+                            <h6>Description : </h6>
+                            <p><?php echo htmlspecialchars($row['bio']); ?></p>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                </tbody>
+            </table>
 		</div>
     </nav>
+
+
+<!--
+============================================
+     liste des amis non encore ajoutés
+============================================
+-->
+
+
     <nav class = "amis" style = "margin-top : 40px;">
         <div id = "friends">
             <h5>Amis en commun</h5>	
         </div>
         <div id="carrousel">
 			<ul id = "listc" style ="list-style-type : none;">
-				<li><img src="images/Celeste.png" width="120" height="100"></li>
-				<li><img src="images/Celeste_LVL8_FaceB.png" width="120" height="100"></li>	
-				<li><img src="images/CelesteScare.png" width="120" height="100"></li>
-				<li><img src="images/CelesteTheo.png" width="120" height="100"></li>
-				<li><img src="chibiartforadrienne" width="120" height="100"></li>
-				<li><img src="images/HollowKnightWallPaper.jfif" width="120" height="100"></li>
-				<li><img src="images/logECE.png" width="120" height="100"></li>
+				<li><img src="images/Celeste.png" alt="pp Ami 1" width="120" height="100"></li>
+				<li><img src="images/Celeste_LVL8_FaceB.png" alt="pp Ami 2" width="120" height="100"></li>
+				<li><img src="images/CelesteScare.png" alt="pp Ami 3" width="120" height="100"></li>
+				<li><img src="images/CelesteTheo.png" alt="pp Ami 4" width="120" height="100"></li>
+				<li><img src="chibiartforadrienne" alt="pp Ami 5" width="120" height="100"></li>
+				<li><img src="images/HollowKnightWallPaper.jfif" alt="pp Ami 6" width="120" height="100"></li>
+				<li><img src="images/logECE.png" alt="pp Ami 7" width="120" height="100"></li>
 				<li><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/StreetMordred.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvU3RyZWV0TW9yZHJlZC5qcGciLCJpYXQiOjE2ODU1NDkyNTYsImV4cCI6MTY4ODE0MTI1Nn0.FOqtr6jvNjSmCcK9k_CeAyBUuo3k_VSmS0VVub_mago&t=2023-05-31T16%3A07%3A38.151Z" width="120" height="100"></li>
-				<li><img src="book9.jpg" width="120" height="100"></li>
-				<li><img src="book10.jpg" width="120" height="100"></li>
-				<li><img src="book11.jpg" width="120" height="100"></li>
-				<li><img src="book12.jpg" width="120" height="100"></li>
+				<li><img src="book9.jpg" alt="pp Ami 9" width="120" height="100"></li>
+				<li><img src="book10.jpg" alt="pp Ami 10" width="120" height="100"></li>
+				<li><img src="book11.jpg" alt="pp Ami 11" width="120" height="100"></li>
+				<li><img src="book12.jpg" alt="pp Ami 12" width="120" height="100"></li>
 			</ul>
 		</div>
 		<div id="buttons">
