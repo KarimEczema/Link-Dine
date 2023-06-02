7d97c27ef356f2cbb6970d03933f69c737a1ca4d
Nirziin <romain.barriere@edu.ece.fr>
2023-06-02 10:27:46 +0200

    Update css
    Suppression de vous.css dans chat. PHP.
    
    "Choisir un fichier" remplacé par une image dans l'accueil pour la création de post.
    
    Restore couleurs navbar.

--- api/accueil.php
+++ api/accueil.php
@@ -26,7 +26,7 @@ include 'caroussel.php';
 
 	<nav class = "section">
 		<div id = "Event">
-			<h5 style = "text-align : center; color:red"> Evennements de la semaine</h5>
+			<h5 style = "text-align : center; color:red"> Evènements de la semaine</h5>
 		</div>
 		<div id="carrousel">
 			<ul id = "listc" style ="list-style-type : none;">
@@ -55,11 +55,12 @@ include 'caroussel.php';
 			<label for="ameliorer">Creer un post</label><br>
 			<div class="container-fluid">
 				<div class="row">
-					 <div class="col-sm-2"><textarea name="ameliorer" id="ameliorer" rows="10" cols="50" style="margin-right: 35px;"></textarea></div>
-					 <div class="col-sm-2">
-						<input type="file" id="image" name="image" accept="image/png, image/jpeg" style = "margin-left : 280% ; margin-top : 90%;">
-						<button type="submit"  style = "margin-left : 280%; margin-top : 10%;">Publier</button>
-					 </div>
+					 <div class="col-sm-7"><textarea name="ameliorer" id="ameliorer" rows="10" cols="50" style="margin-right: 10%;"></textarea></div>
+					<div class="col-sm-2">
+					 	<label for="image_uploads"><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/Photo_site.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvUGhvdG9fc2l0ZS5wbmciLCJpYXQiOjE2ODU2NTA2OTIsImV4cCI6MTY4NjI1NTQ5Mn0.8V7VO2OmDmNFaN6lwNzgsw0zp_qBRhgorvFpWzmQDfc&t=2023-06-01T20%3A18%3A11.492Z"  width="120" height="100" alt="Appareil photo . png"></label>
+    					<input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" style="display:none">
+						<button type="submit"  style = "margin-top : 10%;">Publier</button>
+					</div>
 				</div>
 		</form>
 	</nav>
