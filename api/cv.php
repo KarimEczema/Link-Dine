54286b9cbaba417db856c0f49a5fe2983bbeabe5
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-02 12:53:11 +0200

    Update cv.php

--- api/cv.php
+++ api/cv.php
@@ -1,29 +1,57 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['creerCV']) && $_POST['creerCV'] === 'creationCV') {
-            // The 'creerCV' button with value 'creationCV' was clicked
-            
-            $conn = new PDO($dsn);
-            
-            try {
-                $conn = new PDO($dsn);
-                
-                $sql = "SELECT * FROM formation WHERE iduser = $iduser";
-                $stmt = $conn->query($sql);
-                ?>
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['creerCV']) && $_POST['creerCV'] === 'creationCV') {
+        // The 'creerCV' button with value 'creationCV' was clicked
 
-                <nav style="background-color:grey; padding:2%">
+        $conn = new PDO($dsn);
+
+        try {
+            $conn = new PDO($dsn);
+
+            $sql = "SELECT * FROM formation WHERE iduser = $iduser";
+            $stmt = $conn->query($sql);
+            ?>
+
+            <nav style="background-color:grey; padding:2%">
+
+                <nav class="profil">
+                    <div class="row">
+                        <div class="col-sm-4" style="background-color : aliceblue">Photo</div>
+                        <div class="col-sm-8" style="background-color: grey">
+                            <div style="background-color: #d6a3b7; margin:2%">
+                                <h1>
+                                    <?php echo htmlspecialchars($row['username']); ?>
+                                </h1>
+                                <h3>
+                                    <?php echo htmlspecialchars($row['statut']); ?>
+                                </h3>
+                            </div>
+                            <div style="background-color: #a7d4d4; margin:2%">
+                                <h3>
+                                    <?php echo htmlspecialchars($row['bio']); ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
 
                 <h4 style="margin-top:5%">Formation(s)</h4>
                 <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
                     <div class="row">
-                        <div class="col-sm-3" style="background-color:aliceblue ; margin-left: 10%"><?php echo htmlspecialchars($row['datedebut']); ?> / <?php echo htmlspecialchars($row['datefin']); ?></div>
+                        <div class="col-sm-3" style="background-color:aliceblue ; margin-left: 10%">
+                            <?php echo htmlspecialchars($row['datedebut']); ?> /
+                            <?php echo htmlspecialchars($row['datefin']); ?>
+                        </div>
                         <div class="col-sm-6" style="background-color: grey">
                             <div style=" margin:2%; margin-right: 10%">
-                                <h5><?php echo htmlspecialchars($row['nom']); ?></h5>
+                                <h5>
+                                    <?php echo htmlspecialchars($row['nom']); ?>
+                                </h5>
                             </div>
                             <div style=" margin:2%; margin-right: 10%">
-                                <h6><?php echo htmlspecialchars($row['institution']); ?></h6>
+                                <h6>
+                                    <?php echo htmlspecialchars($row['institution']); ?>
+                                </h6>
                             </div>
                         </div>
                     </div>
@@ -38,10 +66,14 @@
 
                 <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
                     <div class="row">
-                        <div class="col-sm-3" style="background-color:aliceblue ; margin-left: 10%"><?php echo htmlspecialchars($row['nom']); ?></div>
+                        <div class="col-sm-3" style="background-color:aliceblue ; margin-left: 10%">
+                            <?php echo htmlspecialchars($row['nom']); ?>
+                        </div>
                         <div class="col-sm-6" style="background-color: grey">
                             <div style=" margin:2%; margin-right: 10%">
-                                <h5><?php echo htmlspecialchars($row['description']); ?></h5>
+                                <h5>
+                                    <?php echo htmlspecialchars($row['description']); ?>
+                                </h5>
                             </div>
                         </div>
                     </div>
@@ -55,14 +87,15 @@
                 ?>
 
                 <h6 style="margin-top: 2%;">
-                    <?php echo htmlspecialchars("email : " + $row['email']); ?>
+                    <?php echo htmlspecialchars("email : "); ?>
+                    <?php echo htmlspecialchars($row['email']); ?>
                 </h6>
 
-                </nav>
-                <?php
-            } catch (PDOException $e) {
-                echo 'An error occurred: ' . $e->getMessage();
-            }
+            </nav>
+            <?php
+        } catch (PDOException $e) {
+            echo 'An error occurred: ' . $e->getMessage();
         }
     }
-    ?>
+}
+?>
