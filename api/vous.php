7a87e430e02efbc7834b2b9d11a5d2b094807cb1
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-02 10:17:27 +0200

    Update vous.php

--- api/vous.php
+++ api/vous.php
@@ -340,58 +340,6 @@ try {
         </div>
     </div>
 
-    <?php
-    $sql = "SELECT * FROM formations WHERE iduser = $iduser";
-    $conn = new PDO($dsn);
-    $stmt = $conn->query($sql);
-    ?>
-
-        <h4 style="margin-top:5%">Formation(s)</h4>
-        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-            </div >
-            <div class="row">
-                <div class="col-sm-3" style="background-color : purple; margin-left : 10%"><?php echo htmlspecialchars($row['datedebut']); ?> / <?php echo htmlspecialchars($row['datefin']); ?></div>
-                <div class="col-sm-6" style="background-color: red">
-                    <div style="background-color: green; margin:2%; margin-right : 10%">
-                        <h5> <?php echo htmlspecialchars($row['nom']); ?></h5>
-                    </div>
-                    <div style="background-color: yellow; margin:2%; margin-right : 10%">
-                        <h6> <?php echo htmlspecialchars($row['institution']); ?></h6>
-                    </div>
-                </div>
-            </div>
-        <?php endwhile; ?>
-
-    <h4 style="margin-top:5%">Projet(s)</h4>
-
-    <?php
-    $sql = "SELECT * FROM projets WHERE iduser = $iduser";
-    $conn = new PDO($dsn);
-    $stmt = $conn->query($sql);
-    ?>
-
-    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-            <div class="row">
-                <div style="background-color: green; margin-top:2%; margin-left : 10%">
-                    <h5><?php echo htmlspecialchars($row['nom']); ?> -</h5>
-                </div>
-                <div style="background-color: yellow; margin-top:2%; margin-left : 2%; margin-right : 10%">
-                    <h6><?php echo htmlspecialchars($row['description']); ?></h6>
-                </div>
-            </div>
-    <?php endwhile; ?>
-
-
-
-    <?php
-        $sql = "SELECT * FROM users WHERE iduser = $iduser";
-        $conn = new PDO($dsn);
-        $stmt = $conn->query($sql);
-    ?>
-
-    <h6 style="margin-top: 2%;">
-        <?php echo htmlspecialchars($row['email']); ?>
-    </h6>
     }
 </script>
 <button onclick="generer()"> Générer un CV </button>
