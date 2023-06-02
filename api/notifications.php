10ba6f2a152f6b3542cbae514db663638d4277c3
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-02 16:41:31 +0200

    carrousel toutes les images d'un evenement

--- api/notifications.php
+++ api/notifications.php
@@ -1,12 +1,12 @@
 <?php
-    include 'login-check.php';
+include 'login-check.php';
 
 echo '<html>';
 echo '<head>';
 echo '<title>Admin</title>';
-echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'; 
-echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> '; 
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>'; 
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> ';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
 echo '<link rel="stylesheet" type="text/css" href="css/notifications.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/global.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
@@ -19,44 +19,48 @@ include 'caroussel.php';
 
 <body>
 
-<?php
+    <?php
 
-$sql = "SELECT images FROM evenement";
-try {
-    // Création du contact avec la BDD
-    $conn = new PDO($dsn);
-    $stmt = $conn->query($sql);
+    $sql = "SELECT tabimages FROM evenement";
+    try {
+        // Création du contact avec la BDD
+        $conn = new PDO($dsn);
+        $stmt = $conn->query($sql);
 
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-?>
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    ?>
 
-    <nav class = "section">
-		<div id = "Event">
-			<h5 style = "text-align : center; color:red"> Evènements</h5>
-		</div>
-		<div id="carrousel">
-			<ul id = "listc" style ="list-style-type : none;">
+    <nav class="section">
+        <div id="Event">
+            <h5 style="text-align: center; color: red;">Evènements</h5>
+        </div>
+        <div id="carrousel">
+            <ul id="listc" style="list-style-type: none;">
 
-            <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-                <li><img src=<?php echo htmlspecialchars($row['images']); ?> width="120" height="100"></li>
-            <?php endwhile; ?>
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+                    <?php $tabimages = explode(',', $row['tabimages']); ?>
+                    <?php foreach ($tabimages as $image): ?>
+                        <li><img src="<?php echo trim($image); ?>" width="120" height="100"></li>
+                    <?php endforeach; ?>
+                <?php endwhile; ?>
 
-			</ul>
-		</div>
-		<div id="buttons">
-			<input type="button" value="<" class="prev">
-			<input type="button" value=">" class="next">
-		</div>
-	</nav>
+            </ul>
+        </div>
+        <div id="buttons">
+            <input type="button" value="<" class="prev">
+            <input type="button" value=">" class="next">
+        </div>
+    </nav>
 
 
 
 
 
-    <nav class="section" style = "color : black;">
-        <div id = "Amis">
+
+    <nav class="section" style="color : black;">
+        <div id="Amis">
             <h5> Que font mes amis</h5>
         </div>
 
@@ -67,20 +71,21 @@ try {
                 <h6>30/05/2023</h6>
             </div>
             <div class="scroll-page" id="notif-2">
-                <h5>Nom de l'amis  </h5>
+                <h5>Nom de l'amis </h5>
                 <h6>à : Action </h6>
                 <h6>30/05/2023</h6>
             </div>
             <div class="scroll-page" id="notif-3">
-                <h5>Nom de l'amis  </h5>
+                <h5>Nom de l'amis </h5>
                 <h6>à : Action </h6>
                 <h6>29/05/2023</h6>
             </div>
         </div>
-        
+
     </nav>
 
     <?php include 'foot.php' ?>
-    
+
 </body>
+
 </html>
