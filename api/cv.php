5a49349808d9190d1cb41d245f9fde4c69faa3b2
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-02 14:48:54 +0200

    Update cv.php

--- api/cv.php
+++ api/cv.php
@@ -13,27 +13,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <nav style="background-color:grey; padding:2%">
 
-            <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-                <nav class="profil">
-                    <div class="row">
-                        <div class="col-sm-4" style="background-color : aliceblue">Photo</div>
-                        <div class="col-sm-8" style="background-color: grey">
-                            <div style="background-color: #d6a3b7; margin:2%">
-                                <h1>
-                                    <?php echo htmlspecialchars($row['username']); ?>
-                                </h1>
-                                <h3>
-                                    <?php echo htmlspecialchars($row['statut']); ?>
-                                </h3>
-                            </div>
-                            <div style="background-color: #a7d4d4; margin:2%">
-                                <h3>
-                                    <?php echo htmlspecialchars($row['bio']); ?>
-                                </h3>
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+                    <nav class="profil">
+                        <div class="row">
+                            <div class="col-sm-4" style="background-color : aliceblue">Photo</div>
+                            <div class="col-sm-8" style="background-color: grey">
+                                <div style="background-color: #d6a3b7; margin:2%">
+                                    <h1>
+                                        <?php echo htmlspecialchars($row['username']); ?>
+                                    </h1>
+                                    <h3>
+                                        <?php echo htmlspecialchars($row['statut']); ?>
+                                    </h3>
+                                </div>
+                                <div style="background-color: #a7d4d4; margin:2%">
+                                    <h3>
+                                        <?php echo htmlspecialchars($row['bio']); ?>
+                                    </h3>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
                 <?php endwhile; ?>
 
                 <?php
@@ -45,8 +45,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
 
-                
-                
+
+
                     <div class="row">
                         <div class="col-sm-3" style="background-color:aliceblue ; margin-left: 10%">
                             <?php echo htmlspecialchars($row['datedebut']); ?> /
@@ -104,14 +104,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </nav>
             <?php
 
-            $sql = "UPDATE users SET constantecv = 1 WHERE iduser = $iduser";
-            
+            try {
+                $conn = new PDO($dsn);
+
+                // Execute the update query
+                $sql = "UPDATE users SET constantecv = 1 WHERE iduser = $iduser";
+                $conn->exec($sql);
+
+                // Display a success message or perform further actions
+                echo "Update successful!";
+            } catch (PDOException $e) {
+                // Handle any database errors
+                echo "Update failed: " . $e->getMessage();
+            }
+
         } catch (PDOException $e) {
             echo 'An error occurred: ' . $e->getMessage();
         }
     }
 }
 
- include 'foot.php';
+include 'foot.php';
 
 ?>
