34665c7a2afb63befe49806b21a5b3e9894738fa
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-03 20:54:55 +0200

    aaaa

--- api/auteur.php
+++ api/auteur.php
@@ -21,6 +21,23 @@ include 'navbar.php';
 
 <?php
 
+// Include the JWT library
+require __DIR__ . '/vendor/autoload.php';
+
+
+$host = "ep-twilight-term-343583-pooler.eu-central-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "Y4vuPQm2xyTl";
+
+$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+
+
+
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
 try{
     if($_POST)
     {
@@ -97,7 +114,7 @@ catch(PDOException $e){
         <label for="ameliorer">Creer un post</label><br>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-7"><textarea name="ameliorer" id="ameliorer" rows="10" cols="50" style="margin-right: 10%;" required></textarea></div>
+                <div class="col-sm-7"><textarea name="write" id="write" cols = "50" rows = "10" wrap="hard" required></textarea></div>
                 <div class="col-sm-5">
                     <label for="image_uploads"><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/Photo_site.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvUGhvdG9fc2l0ZS5wbmciLCJpYXQiOjE2ODU2NTA2OTIsImV4cCI6MTY4NjI1NTQ5Mn0.8V7VO2OmDmNFaN6lwNzgsw0zp_qBRhgorvFpWzmQDfc&t=2023-06-01T20%3A18%3A11.492Z"  width="120" height="100" alt="Appareil photo . png"></label>
                     <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" style="display:none">
@@ -106,29 +123,25 @@ catch(PDOException $e){
                         <p>A qui voulez vous le partager ?</p>
 
                         <div>
-                            <input type="radio" id="huey" name="drone" value="huey" checked>
+                            <input type="radio" id="friend" name="secu" value="friend" checked>
                         <label for="huey">Vos amis</label>
                         </div>
 
                         <div>
-                            <input type="radio" id="dewey" name="drone" value="dewey">
+                            <input type="radio" id="all" name="secu" value="all">
                             <label for="dewey">Tout le monde</label>
                         </div>
                     </fieldset>
                 </div>
             </div>
         </div>
+        <label for="start">Quand ?</label>
+        <input type="date" id="date" name="date" value="2023-03-22" min="2015-01-01" max="2026-12-31" style = "text-align : left">
+        <label for="where"style = "text-align : right;">Où ?</label>
+        <input type="text" id="lieu" name="lieu" style = "margin-left : 10%;">
     </form>
-    <label for="start">Quand ?</label>
-    <input type="date" id="start" name="trip-start" value="2023-03-22" min="2015-01-01" max="2026-12-31" style = "text-align : left">
-    <label for="where"style = "text-align : right;">Où ?</label>
-    <input type="text" id="where" name="trip-start" style = "margin-left : 10%;">
 </nav>
 
 <nav class = "like" style =" background-color: bisque;">
    <h5> Que vous pourriez aimer :</h5><br>
-</nav><div>
-
-
-    </main>
-</div>
+</nav>
