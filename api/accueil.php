1e8fc93839393d98dbdee9e5189765b7dc3699b1
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-03 23:26:07 +0200

    Update accueil.php

--- api/accueil.php
+++ api/accueil.php
@@ -154,7 +154,11 @@ try {
 
                                     <?php
                                     echo "<div>";
-                                    echo "<h2>" . htmlspecialchars($item['title']) . "</h2>";
+                                    if($item['title'] !== NULL)
+                                    {
+                                        echo "<h2>" . htmlspecialchars($item['title']) . "</h2>";
+
+                                    }
                                     echo "<p>Publication date: " . $item['datepublication'] . "</p>";
                                     echo "<p>" . htmlspecialchars($item['description']) . "</p>";
                                     echo "</div>";
