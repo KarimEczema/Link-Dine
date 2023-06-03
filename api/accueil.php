d82e59f81735414e586d400a5c99c35c82c69758
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-03 21:04:53 +0200

    overlapping accueil

--- api/accueil.php
+++ api/accueil.php
@@ -54,31 +54,35 @@ $decoded_images = json_decode($row['tabimages'], true); // decode the JSON strin
 
 <div style="border:solid;">
 
-<h3 style="text-align: center; margin:3%; text-decoration:underline;">Evénement de la semaine :</h3>
-<h2 style="text-align: center; margin:1%"><?php echo htmlspecialchars($row2['nom']); ?></h2>
-<h3 style="text-align: center; margin:1%"><?php echo htmlspecialchars($row2['organisateur']); ?></h3>
+    <h3 style="text-align: center; margin:3%; text-decoration:underline;">Evénement de la semaine :</h3>
+    <h2 style="text-align: center; margin:1%">
+        <?php echo htmlspecialchars($row2['nom']); ?>
+    </h2>
+    <h3 style="text-align: center; margin:1%">
+        <?php echo htmlspecialchars($row2['organisateur']); ?>
+    </h3>
 
 
 
-<div class="carousel" id="test1">
-    <?php
-    $valueCar = 1;
-    $tabimages = explode(',', $row['tabimages']);
-    ?>
-    <?php foreach ($tabimages as $image):
-        if ($valueCar == 1) { ?>
-            <input type="radio" name="item" value="<?php echo $valueCar; ?>" checked>
-            <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
-            <?php $valueCar++;
-        } else { ?>
-            <input type="radio" name="item" value="<?php echo $valueCar; ?>">
-            <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
-            <?php
-            $valueCar++;
-        }
+    <div class="carousel" id="test1">
+        <?php
+        $valueCar = 1;
+        $tabimages = explode(',', $row['tabimages']);
         ?>
-    <?php endforeach; ?>
-</div>
+        <?php foreach ($tabimages as $image):
+            if ($valueCar == 1) { ?>
+                <input type="radio" name="item" value="<?php echo $valueCar; ?>" checked>
+                <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
+                <?php $valueCar++;
+            } else { ?>
+                <input type="radio" name="item" value="<?php echo $valueCar; ?>">
+                <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
+                <?php
+                $valueCar++;
+            }
+            ?>
+        <?php endforeach; ?>
+    </div>
 
 </div>
 
@@ -179,60 +183,70 @@ try {
 <h1 style="padding:10% ">Mes événements</h1>
 
 <?php
-    
-    $sql = "SELECT * FROM evenement";
-	try{
+
+$sql = "SELECT * FROM evenement";
+try {
     // Création du contact avec la BDD
-            $conn = new PDO($dsn);
-            $stmt = $conn->query($sql);
+    $conn = new PDO($dsn);
+    $stmt = $conn->query($sql);
 
-	}catch (PDOException $e){
-    	echo $e->getMessage();
-	}
-    ?>
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
 
-<nav class = "myEvents">  
-    <div class="scroll-container"> 
+<nav class="myEvents">
+    <div class="scroll-container">
         <table>
             <tbody>
-                <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
 
-                    <div class="scroll-page" id="eventperso"> 
+                    <div class="scroll-page" id="eventperso">
                         <div style="margin:2%; border:solid;">
-                        <h5><B><?php echo htmlspecialchars($row['nom']); ?></B>   <?php echo htmlspecialchars($row['date']); ?></h5> 
-                        <h6>Type de contrat : <?php echo htmlspecialchars($row['organisateur']); ?></h6> <br> 
-                        <h6>
-                            <div class="open-btn">
-                                <button class="open-button" onclick="openForm(<?php echo $row['idevent']?>)"><strong>Description de l'événement</strong></button>
-                            </div> 
-                        </h6> 
-                        <div class="login-popup">
-                            <div class="Description" id="form-<?php echo $row['idevent'];?>">
-                                <div class="descr-container">
-                                    <h4>Description de l'événement :</h4>
-                                    <?php echo htmlspecialchars($row['description']); ?>
-                                    <button type="button" class="btn cancel" onclick="closeForm(<?php echo $row['idevent']?>)" style="background-color: antiquewhite">Fermer</button>
+                            <h5><B>
+                                    <?php echo htmlspecialchars($row['nom']); ?>
+                                </B>
+                                <?php echo htmlspecialchars($row['date']); ?>
+                            </h5>
+                            <h6>Organisateur :
+                                <?php echo htmlspecialchars($row['organisateur']); ?>
+                            </h6> <br>
+                            <h6>
+                                <div class="open-btn">
+                                    <button class="open-button"
+                                        onclick="openForm(<?php echo $row['idevent'] ?>)"><strong>Description de
+                                            l'événement</strong></button>
+                                </div>
+                            </h6>
+                            <div class="login-popup">
+                                <div class="Description" id="form-<?php echo $row['idevent']; ?>">
+                                    <div class="descr-container">
+                                        <h4>Description de l'événement :</h4>
+                                        <?php echo htmlspecialchars($row['description']); ?>
+                                        <button type="button" class="btn cancel"
+                                            onclick="closeForm(<?php echo $row['idevent'] ?>)"
+                                            style="background-color: antiquewhite">Fermer</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        </div>
-                    </div> 
+                    </div>
 
                     <script>
                         function openForm(id) {
-                            document.getElementById("form-"+id).style.display = "block";
+                            document.getElementById("form-" + id).style.display = "block";
                         }
 
                         function closeForm(id) {
-                            document.getElementById("form-"+id).style.display = "none";
+                            document.getElementById("form-" + id).style.display = "none";
                         }
                     </script>
 
                 <?php endwhile; ?>
             </tbody>
         </table>
-    </div> 
-</nav> 
+    </div>
+</nav>
 
 
 <?php include 'foot.php'; ?>
