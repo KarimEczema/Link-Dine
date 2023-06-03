b53ded6a97ce3209201c705a613dd026209c9c1d
GauthierMallard <gauthier.mallard@edu.ece.fr>
2023-06-03 13:52:29 +0200

    Test de maj globale de fond

--- api/login-check.php
+++ api/login-check.php
@@ -53,8 +53,9 @@ if (isset($_COOKIE['jwt'])) {
         $fondbg = $stmt->fetch(PDO::FETCH_ASSOC);
 
         echo' <script type="text/javascript">';
-        echo'    let jsVar = "' . $fondbg . '";'; // Now the PHP variable has been assigned to the JavaScript variable'
-        echo'    document.body.style.background-color = jsVar;'; // Changing the bodys background color using JavaScript
+        echo'     let jsVar = "' . $fondbg . '";'; // assignation de la variable php à la variable JavaScript
+        echo'     let stylebg = document.getElementById("bg") ';
+        echo'     stylebg.innerHTML = "body { backround-color:  $fonbg }"';
         echo' </script>';
 
 } catch (Exception $e) {
