038a8567564f5b8a125b079e7e6b9cb99e5072a5
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-03 10:53:15 +0200

    Update notifications.php

--- api/notifications.php
+++ api/notifications.php
@@ -7,12 +7,9 @@ echo '<title>Admin</title>';
 echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
 echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> ';
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
-echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';
 echo '<link rel="stylesheet" type="text/css" href="css/notifications.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/global.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
-
-
 echo '<body>';
 
 include 'navbar.php';
@@ -36,54 +33,23 @@ include 'caroussel.php';
     ?>
 
     <?php $row['tabimages'] = trim($row['tabimages'], '{}'); // remove the starting and ending curly braces
-    $tabimages = explode(',', $row['tabimages']);
-    $decoded_images = json_decode($row['tabimages'], true);  // decode the JSON string to an associative array ?>
-    <script>console.log($decoded_images)</script>
+    $decoded_images = json_decode($row['tabimages'], true); // decode the JSON string to an associative array ?>
 
     <nav class="section">
         <div id="Event">
-            <h5 style="text-align: center; color: red;">Evénements</h5>
+            <h5 style="text-align: center; color: red;">Evènements</h5>
         </div>
 
-        <div class="carousel slide" id="myCarousel" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <?php
-                $valueCar = 0;
-
-                foreach ($decoded_images as $image) {
-                    echo '<li data-target="#myCarousel" data-slide-to="' . $valueCar . '"';
-                    if ($valueCar === 0) {
-                        echo ' class="active"';
-                    }
-                    echo '></li>';
-                    $valueCar++;
-                }
-                ?>
-            </ol>
-
-            <div class="carousel-inner">
-                <?php
-                $valueCar = 0;
-                foreach ($decoded_images as $image) {
-                    echo '<div class="carousel-item';
-                    if ($valueCar === 0) {
-                        echo ' active';
-                    }
-                    echo '">';
-                    echo '<img src="' . trim($image) . '" alt="Carousel Image">';
-                    echo '</div>';
-                    $valueCar++;
-                }
-                ?>
-            </div>
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+        <div class="carousel" id="test1">
+            <?php
+            $valueCar = 1;
+            $tabimages = explode(',', $row['tabimages']);
+            ?>
+            <?php foreach ($tabimages as $image): ?>
+                <input type="radio" name="item" value="<?php echo $valueCar; ?>">       
+                    <img src="<?php echo trim($image); ?>" style="height : 60px; width : 60px">
+                <?php $valueCar++; ?>
+            <?php endforeach; ?>
         </div>
     </nav>
 
