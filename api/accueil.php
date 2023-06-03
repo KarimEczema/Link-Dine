c96d08b92e705b67ad23cce0a232597bd52ec6c2
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-03 20:52:59 +0200

    Update accueil.php

--- api/accueil.php
+++ api/accueil.php
@@ -198,7 +198,7 @@ try {
                 <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
 
                     <div class="scroll-page" id="eventperso"> 
-                        <div style="margin:2%">
+                        <div style="margin:2%; border:solid;">
                         <h5><B><?php echo htmlspecialchars($row['nom']); ?></B>   <?php echo htmlspecialchars($row['date']); ?></h5> 
                         <h6>Type de contrat : <?php echo htmlspecialchars($row['organisateur']); ?></h6> <br> 
                         <h6>
