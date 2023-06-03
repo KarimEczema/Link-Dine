6b76e3b310c9734cc534f6c4f8ce4d6ce8a8552f
Nirziin <romain.barriere@edu.ece.fr>
2023-06-03 14:05:40 +0200

    Correction de input text et de form

--- api/auteur.php
+++ api/auteur.php
@@ -69,11 +69,11 @@ catch(PDOException $e){
 
 
 <nav class = "post" style =" background-color: cyan;">
-    <form method="post" action="auteur.php">
+    <form method="post" action="">
         <label for="ameliorer">Creer un post</label><br>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-7"><textarea name="write" id="write" rows="10" cols="50" style="margin-right: 10%;" required></textarea></div>
+                <div class="col-sm-7"><textarea rows="10" cols="50" style="margin-right: 10%;" required><input type = "text" name="write" id="write" ></textarea></div>
                 <div class="col-sm-5">
                     <label for="image_uploads"><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/Photo_site.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvUGhvdG9fc2l0ZS5wbmciLCJpYXQiOjE2ODU2NTA2OTIsImV4cCI6MTY4NjI1NTQ5Mn0.8V7VO2OmDmNFaN6lwNzgsw0zp_qBRhgorvFpWzmQDfc&t=2023-06-01T20%3A18%3A11.492Z"  width="120" height="100" alt="Appareil photo . png"></label>
                     <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" style="display:none">
