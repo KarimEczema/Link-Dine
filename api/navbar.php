15cb033b506a1eb9c4338c77f05faa59312ede2f
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 23:25:43 +0200

    Logo

--- api/navbar.php
+++ api/navbar.php
@@ -2,7 +2,7 @@
 		<div class="container-fluid">
 			<div class="row">
 				 <div class="col-sm-8"><h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3></div>
-				 <div class="col-sm" ><img src="image" width="121" height="49.5"></div>
+				 <div class="col-sm" ><img src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Image/LogECE.png" width="121" height="49.5"></div>
 			</div>		
 		</div>
 </header>
