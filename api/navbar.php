88174815e0b25677c54e5acb798d72b753677b62
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 11:32:28 +0200

    #ad
    Ajout de publicité

--- api/navbar.php
+++ api/navbar.php
@@ -22,3 +22,9 @@
 		<?php endif; ?>
 	</ul>
 </nav>
+
+
+<div id = "ad">
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3533297835167860" crossorigin="anonymous">
+	</script>
+</div>
