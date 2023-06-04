4c2044a4d5752a172c13c0d2f8d5f820cf0c244d
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-04 16:47:38 +0200

    Update notifications.php

--- api/notifications.php
+++ api/notifications.php
@@ -97,7 +97,7 @@ include 'navbar.php';
                             <?php
                             foreach ($combined as $item) {
 
-                                if ($item['username'] == 'tous') { ?>
+                                if ($item['accessibilite'] == 'tous') { ?>
                                     <div class="scroll-page" id="eventPerso">
                                         <div style="padding:2%; border:solid;">
                                             <?php
