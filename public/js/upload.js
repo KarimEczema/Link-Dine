9bde77c8ce75e997cc0257c51813264941c61601
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-04 11:28:13 +0200

    Update upload.js

--- public/js/upload.js
+++ public/js/upload.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://bmqgiyygwjnnfyrtjkno.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcWdpeXlnd2pubmZ5cnRqa25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNzM1NzcsImV4cCI6MjAwMDk0OTU3N30.sQgvRElC6O5e4uE8OVZqLXBiQYQa83mSkTy4s4L0aDw'
 
-const supabase = createClient(supabaseUrl,supabaseAnonKey);
+var supabase = new Supabase.SupabaseClient(supabaseUrl, supabaseAnonKey);
 
 
 document.getElementById('image_uploads').addEventListener('change', function() {
