021deafe61122c84b1eb52a90aaa6dc30ba514e8
Gwendera <adrienne.vidon@edu.ece.fr>
2023-06-04 11:42:09 +0200

    Update notifications.php

--- api/notifications.php
+++ api/notifications.php
@@ -64,6 +64,124 @@ include 'caroussel.php';
 
 
     <?php include 'eventamis.php'; ?>
+
+<!--
+=====================================================================================
+        Partie Evénements des amis de mes amis (nouveaux posts/formations/projets)
+=====================================================================================
+-->
+
+<h1 style="padding:10%">Evénements de mes amis</h1>
+<?php
+try {
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+
+    // query to check if username exists
+    $sql = "SELECT amis FROM users WHERE iduser = ?";
+    $stmt = $conn->prepare($sql);
+
+    // bind parameters and execute
+    $stmt->execute([$iduser]);
+
+    $amis = $stmt->fetch();
+
+    if ($amis && $amis['amis'] !== null) {
+        $amis = explode(',', trim($amis['amis'], '{}')); // convert the array string into a PHP array
+
+        // Check that the user has friends
+        if (!empty($amis)) {
+            $combined = [];
+
+            foreach ($amis as $ami) {
+                // Get the friends of the current friend
+                $stmt = $conn->prepare("SELECT amis FROM users WHERE iduser = ?");
+                $stmt->execute([$ami]);
+                $friends = $stmt->fetch();
+
+                if ($friends && $friends['amis'] !== null) {
+                    $friends = explode(',', trim($friends['amis'], '{}')); // convert the array string into a PHP array
+
+                    // Combine the friends' IDs with the current friend's ID
+                    $friendIds = array_merge([$ami], $friends);
+
+                    foreach ($friendIds as $friendId) {
+                        // Get posts
+                        $stmt = $conn->prepare("SELECT users.nom as username, lieu as title, date, descriptionpost as description, datepublication FROM posts INNER JOIN users ON posts.iduser = users.iduser WHERE posts.iduser = ? ORDER BY datepublication DESC");
+                        $stmt->execute([$friendId]);
+                        $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                        // Get formations
+                        $stmt = $conn->prepare("SELECT users.nom as username, formation.nom as title, (datedebut || ', ' || datefin) as date, institution as description, datepublication FROM formation INNER JOIN users ON formation.iduser = users.iduser WHERE formation.iduser = ? ORDER BY datepublication DESC");
+                        $stmt->execute([$friendId]);
+                        $formations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                        // Get projects
+                        $stmt = $conn->prepare("SELECT users.nom as username, projet.nom as title, NULL as date, description, datepublication FROM projet INNER JOIN users ON projet.iduser = users.iduser WHERE projet.iduser = ? ORDER BY datepublication DESC");
+                        $stmt->execute([$friendId]);
+                        $projets = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                        // Combine all and sort by datepublication
+                        $combined = array_merge($combined, $posts, $formations, $projets);
+                    }
+                }
+            }
+
+            usort($combined, function ($a, $b) {
+                return $b['datepublication'] <=> $a['datepublication'];
+            });
+
+            ?>
+
+            <div class="scroll-container">
+                <table>
+                    <tbody>
+                    <?php
+                    foreach ($combined as $item) {
+                        ?>
+                        <div class="scroll-page" id="eventperso">
+                            <div style="padding:2%; border:solid;">
+                                <?php
+                                echo "<div>";
+                                if ($item['title'] !== NULL) {
+                                    echo "<h2>" . htmlspecialchars($item['title']) . "</h2>";
+                                }
+                                echo "<p>Posté par: " . htmlspecialchars($item['username']) . "</p>";
+                                echo "<h6 style='font-style:italic'>Date de publication: " . htmlspecialchars($item['datepublication']) . "</h6>";
+                                echo "<h6>" . htmlspecialchars($item['description']) . "</h6>";
+                                echo "</div>";
+                                ?>
+                            </div>
+
+                            <script>
+                                function openForm(id) {
+                                    document.getElementById("form-" + id).style.display = "block";
+                                }
+
+                                function closeForm(id) {
+                                    document.getElementById("form-" + id).style.display = "none";
+                                }
+                            </script>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+            <?php
+
+        } else {
+            echo "This user has no friends.";
+        }
+    } else {
+        echo "This user has no friends.";
+    }
+} catch (PDOException $e) {
+    // report error message
+    echo $e->getMessage();
+}
+?>
+
     <?php include 'foot.php' ?>
 
 </body>
