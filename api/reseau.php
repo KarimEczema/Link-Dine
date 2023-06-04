82512627631742398581f4675f48c5cd10a760ee
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 15:50:37 +0200

    Remise des bonnes versions jquery et bs

--- api/reseau.php
+++ api/reseau.php
@@ -4,24 +4,19 @@ echo '<html>';
 echo '<head>';
 echo '<title>Réseau</title>';
 
-
-// Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
-echo '<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">';
-echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>';
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> ';
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
+echo '<link rel="stylesheet" type="text/css" href="css/vous.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/global.css">';
-echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
-echo '<link rel="stylesheet" type="text/css" href="css/reseau.css">';
-
 
 include 'login-check.php';
-?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3533297835167860" crossorigin="anonymous"></script>
 
-</head>
-<?php
+// Ici, we're adding the links to Bootstrap CSS and jQuery via their CDNs
+
+
+echo '</head>';
 echo '<body>';
-
 ?>
 <nav class = "bg">
 <?php
