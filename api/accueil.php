9ce7b1ef99831768ac516bfa77593c6a05953f4b
KarimEczema <elhousnyadel@gmail.com>
2023-06-04 22:48:13 +0200

    Update accueil.php

--- api/accueil.php
+++ api/accueil.php
@@ -224,10 +224,10 @@ try {
                                         <!-- Partie Commentaire -->
                                         <h6>
                                             <div class="open-btn">
-                                                <button class="open-button" onclick="ouvrcommentaire(<?php echo $row['idemploi'];?>)">Commenter </button>
+                                                <button class="open-button" onclick="ouvrcommentaire(<?php echo $row['idpost'];?>)">Commenter </button>
                                             </div>
                                             <div class="open-btn">
-                                                <button class="open-button" onclick="ouvrtabcommentaire(<?php echo $row['idemploi'];?>)">Commentaires </button>
+                                                <button class="open-button" onclick="ouvrtabcommentaire(<?php echo $row['idpost'];?>)">Commentaires </button>
                                             </div>
                                         </h6>
 
