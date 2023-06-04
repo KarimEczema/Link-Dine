eb2583321891dc7bb8c0959c185e0646a4006a1d
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 11:46:23 +0200

    #ad dans head

--- api/accueil.php
+++ api/accueil.php
@@ -1,151 +1,114 @@
 <?php
 
+include 'login-check.php';
+
 echo '<html>';
 echo '<head>';
 echo '<title>Accueil</title>';
 
 // Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
 echo '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>';
-echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
+echo "Logged in as: " . $iduser;
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'; 
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>'; 
 echo '<link rel="stylesheet" type="text/css" href="css/accueil.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/global.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
-include 'login-check.php';
-echo '</head>';
 echo '<body>';
 
 include 'navbar.php';
 include 'caroussel.php';
+include 'pub.php';
+echo '</head>'
 ?>
 
 <?php
-$sql = "SELECT tabimages
+    $sql = "SELECT tabimages
 	FROM evenement
 	WHERE DATE(date) >= '2023-06-05'
 	  AND DATE(date) <= '2023-06-11';
 	";
-try {
-    // Création du contact avec la BDD
-    $conn = new PDO($dsn);
-    $stmt = $conn->query($sql);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-?>
-
-<?php $row['tabimages'] = trim($row['tabimages'], '{}'); // remove the starting and ending curly braces
-$decoded_images = json_decode($row['tabimages'], true); // decode the JSON string to an associative array ?>
-
-<h5 style="text-align: center; color: red; margin:3%">Evénement de la semaine :</h5>
-
-<div class="carousel" id="test1">
-    <?php
-    $valueCar = 1;
-    $tabimages = explode(',', $row['tabimages']);
+    try {
+        // Création du contact avec la BDD
+        $conn = new PDO($dsn);
+        $stmt = $conn->query($sql);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
     ?>
-    <?php foreach ($tabimages as $image):
-        if ($valueCar == 1) { ?>
-            <input type="radio" name="item" value="<?php echo $valueCar; ?>" checked>
-            <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
-            <?php $valueCar++;
-        } else { ?>
-            <input type="radio" name="item" value="<?php echo $valueCar; ?>">
-            <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
-            <?php
-            $valueCar++;
-        }
-        ?>
-    <?php endforeach; ?>
-</div>
 
-<!--
+    <?php $row['tabimages'] = trim($row['tabimages'], '{}'); // remove the starting and ending curly braces
+    $decoded_images = json_decode($row['tabimages'], true); // decode the JSON string to an associative array ?>
+
+    <h5 style="text-align: center; color: red; margin:3%">Evénement de la semaine :</h5>
+
+    <div class="carousel" id="test1">
+        <?php
+        $valueCar = 1;
+        $tabimages = explode(',', $row['tabimages']);
+        ?>
+        <?php foreach ($tabimages as $image):
+            if ($valueCar == 1) { ?>
+                <input type="radio" name="item" value="<?php echo $valueCar; ?>" checked> 
+                <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
+                <?php $valueCar++;
+            } else { ?>
+                <input type="radio" name="item" value="<?php echo $valueCar; ?>"> 
+                <div><img src="<?php echo trim($image); ?>" style="height : 350px; width : 600px"></div>
+                <?php
+                $valueCar++;
+            }
+            ?>
+        <?php endforeach; ?>
+    </div>
+
+    	<!--
 ----------   Affichage    ----------
 -->
 
 
 <h1 style="padding:10% ">Time Line</h1>
 <?php
-try {
-    // create a PostgreSQL database connection
-    $conn = new PDO($dsn);
+    $sql = "SELECT amis FROM users WHERE iduser = $iduser";
+    try {
+        // create a PostgreSQL database connection
+        $conn = new PDO($dsn);
+        $ami = $conn->prepare($sql);
 
-    // query to check if username exists
-    $sql = "SELECT amis FROM users WHERE iduser = ?";
-    $stmt = $conn->prepare($sql);
+        // bind parameters and execute
+        $ami->execute([$iduser]);
 
-    // bind parameters and execute
-    $stmt->execute([$iduser]);
+    } catch (PDOException $e) {
+        // report error message
+        echo $e->getMessage();
+    }
 
-    $amis = $stmt->fetch();
+    // Check that the user has friends
+    if ($ami!=NULL) {
+        while($ami = $stmt->fetch(PDO::FETCH_ASSOC)) :
+            
+                $ami = explode(',', trim($ami['amis'], '{}')); // convert the array string into a PHP array
 
-    if ($amis && $amis['amis'] !== null) {
-        $amis = explode(',', trim($amis['amis'], '{}')); // convert the array string into a PHP array
+                // Retrieve the friends' posts
+                $params = implode(',', array_fill(0, count($ami), '?'));
+                $stmt = $conn->prepare("SELECT * FROM posts WHERE iduser IN ($params)");
+                $stmt->execute($ami);
+                $posts = $stmt->fetchAll();
 
-
-        // Check that the user has friends
-        if (!empty($amis)) {
-            // Retrieve the friends' posts
-            $params = implode(',', array_fill(0, count($amis), '?'));
-
-            $stmt = $conn->prepare("SELECT * FROM posts WHERE iduser IN ($params)");
-            $stmt->execute($amis);
-            $posts = $stmt->fetchAll(); ?>
-
-
-            <div class="scroll-container">
-                <table>
-                    <tbody>
-                        <?php
-
-                        // Display the posts
-                        foreach ($posts as $post) {
-
-                            $temp = $post['idpost'];
-
-                            $sql2 = "SELECT u.nom
-            FROM users u
-            JOIN posts p ON u.iduser = p.iduser
-            WHERE p.idpost = ?";
-
-                            $stmt2 = $conn->prepare($sql2);
-                            $stmt2->execute([$temp]);
-                            $result = $stmt2->fetch();
-                            ?>
-
-                            <div class="scroll-page" id="formation">
-                                <div class="col-sm-4" style="background-color:#d6a3b7">
-                                    <?php echo $result['nom']; ?>
-                                </div>
-                                <div class="col-sm-8" style="background-color:#a7d4d4">
-                                    <?php echo htmlspecialchars($post['descriptionpost']); ?>
-                                </div>
-                            </div>
-
-
-                            <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-            <?php
-
-        } else {
-            echo "This user has no friends.";
-        }
+                // Display the posts
+                
+                    foreach ($posts as $post) {
+                        echo "ID: " . $post[$ami] . ", Content: " . $post['descriptionpost'] . "<br>";
+                    }
+                
+            
+        endwhile;
     } else {
         echo "This user has no friends.";
     }
-
-} catch (PDOException $e) {
-    // report error message
-    echo $e->getMessage();
-}
 ?>
 
-<?php include 'foot.php'; ?>
+	<?php include 'foot.php';?>
 </body>
-
 </html>
---
+++ api/pub.php
@@ -0,0 +1,5 @@
+<div id = "ad">
+	<h1> Pub : </h1>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3533297835167860" crossorigin="anonymous">
+	</script>
+</div>
