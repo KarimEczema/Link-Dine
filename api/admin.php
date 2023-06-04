6fe9bcd43bc1b9d68944dfccc1027f119ae2e78f
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 22:37:51 +0200

    Décalage reset

--- api/admin.php
+++ api/admin.php
@@ -71,11 +71,11 @@ try {
 
 <nav class = "Ajout-compte">
     <h1 style = "margin-top : 5% ">Ajouter un utilisateur</h1>
-    <form method="post" action="">
+    <form name ="ajout" method="post" action="">
     <div style = "background-color: grey; margin-top:2%" ><h5>Pseudo : <input type="text" name="nom" style="margin : 5%" required> </h5></div>
     <div style = "background-color: grey; margin:2%"><h5>Email : <input type="text" name="email" style="margin : 5%" required> </h5></div>
 
-    <input type="submit"  style = "margin-top : 2%;" name="ajouter" value="Creer" onclick="this.form.reset();">
+    <input type="submit"  style = "margin-top : 2%;" name="ajouter" value="Creer">
     </form>
 </nav>
 
@@ -103,6 +103,7 @@ if($_POST){
 
         //Message de confirmation pour l'utilisateur
         echo "Utilisateur ajouté !";
+        echo 'ajout.form.reset()';
     }
 }
 }catch (PDOException $e){
