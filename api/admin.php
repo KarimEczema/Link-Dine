f6686eed5e8c2af24337fa89eb3c11a3f2a97d04
Nirziin <romain.barriere@edu.ece.fr>
2023-06-04 22:50:12 +0200

    id?

--- api/admin.php
+++ api/admin.php
@@ -71,7 +71,7 @@ try {
 
 <nav class = "Ajout-compte">
     <h1 style = "margin-top : 5% ">Ajouter un utilisateur</h1>
-    <form name ="ajout" method="post" action="">
+    <form id ="ajout" method="post" action="">
     <div style = "background-color: grey; margin-top:2%" ><h5>Pseudo : <input type="text" name="nom" style="margin : 5%" required> </h5></div>
     <div style = "background-color: grey; margin:2%"><h5>Email : <input type="text" name="email" style="margin : 5%" required> </h5></div>
 
