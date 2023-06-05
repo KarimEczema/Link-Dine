e8c31eb6bd13a91dadfb7628f57e8df84ea4236f
KarimEczema <elhousnyadel@gmail.com>
2023-06-05 15:35:46 +0200

    video fix

--- api/accueil.php
+++ api/accueil.php
@@ -19,6 +19,13 @@ echo '<body>';
 <nav class="bg">
 	<?php
 	include 'navbar.php';
+	?>
+		<div class="video-container">
+			<video
+				src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Video/ECE%20undefined.mp4?t=2023-06-05T13%3A21%3A46.734Z"
+				autoplay muted loop></video>
+		</div>
+	<?php
 	include 'caroussel.php';
 	?>
 
