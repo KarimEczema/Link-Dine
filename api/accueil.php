4d5e661c8971f5e8f826d704e1da1b284ce7b3da
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 01:30:36 +0200

    like

--- api/accueil.php
+++ api/accueil.php
@@ -11,6 +11,7 @@ echo '<script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/loc
 echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
 echo '<script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=647c5fc840353a0019caf23d&product=sop" async="async"></script>';
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />';
 echo '<link rel="stylesheet" type="text/css" href="css/accueil.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/global.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
@@ -203,7 +204,9 @@ WHERE DATE(date) >= '2023-06-05'
 										<!-- Partie like -->
 										<form>
 											<button type="submit" id="like-<?php echo $idpost; ?>" name="ajouterlike"
-												style="margin-top:10%; margin-left:3%;" class="like-button">like</button>
+												style="margin-top: 10%; margin-left: 3%;" class="like-button">
+												<i class="fas fa-heart"></i> <!-- Heart icon -->
+											</button>
 										</form>
 
 										<script>
