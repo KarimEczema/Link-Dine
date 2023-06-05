664e18c1bb794db7ad891454f86e4f2e57277eac
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-05 18:50:46 +0200

    TEst scrolll barsss + newnavbar

--- api/navbar.php
+++ api/navbar.php
@@ -1,17 +1,10 @@
-<header>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-8">
-				<h3>ECE-in : Social Media Professionnel de l'ECE Paris</h3>
-			</div>
-			<div class="col-sm"><img
-					src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Image/LogECE.png"
-					width="121" height="49.5"></div>
-		</div>
-	</div>
-</header>
-
 <nav class="navigation">
+
+	<div class="nav-left">
+	<a href="acceuil"><img
+            src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Image/LogECE.png?t=2023-06-05T16%3A49%3A58.325Z"
+            ></a>
+        </div>
 	<ul id="liste1">
 		<li id="accueil"><a href="accueil">Accueil</a></li>
 		<li id="reseau"><a href="reseau">Mon réseau</a></li>
