e27e464591ba23ec2e0d6e7842378e94265281fa
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 04:48:08 +0200

    Update accueil.php

--- api/accueil.php
+++ api/accueil.php
@@ -161,7 +161,7 @@ WHERE DATE(date) >= '2023-06-05'
 							foreach ($posts as $item) {
 								?>
 								<div class="scroll-page" id="eventperso">
-									<div style="padding:2%; border: 2px solid rgb(107, 107, 107);background-color: #E5F0FF;
+									<div style="padding:2%; border: 1px solid #CACACA;background-color: #E5F0FF;
   /*border: 2px solid #666;*/
   border-radius: 10px;">
 
