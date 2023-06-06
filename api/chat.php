3e3ffb78568ea770a34fc9c2110b4268d0776fab
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 07:03:33 +0200

    Update chat.php

--- api/chat.php
+++ api/chat.php
@@ -34,8 +34,7 @@ include 'navbar.php';
             <div id="buttonArea" style="position: fixed; bottom: 0;">
                 <button id="messageButton" class="btn btn-primary"><i class="fas fa-comment-dots"></i></button>
                 <button id="videoCallButton" class="btn btn-primary"><i class="fas fa-video"></i></button>
-                <button id="endCallButton" class="btn btn-primary"><i class="fas fa-phone-slash"></i></button>
-                <!-- Bouton de fin d'appel -->
+                <button id="endCallButton" class="btn btn-primary"><i class="fas fa-video"></i></button>
             </div>
         </div>
     </div>
