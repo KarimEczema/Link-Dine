728941bfb01cb51c54cb888a74d0a37259b71ce9
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 04:11:00 +0200

    Update foot.php

--- api/foot.php
+++ api/foot.php
@@ -24,23 +24,23 @@
             </div>
             <div class="col-sm-12">
                 <nav class="navigation">
-                    div class="nav-left">
-                    <a href="accueil"><img
-                            src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Image/LOGO.png?t=2023-06-05T17%3A06%3A28.235Z"></a>
+                    <div class="nav-left">
+                        <a href="accueil"><img
+                                src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/public/Images/Image/LOGO.png?t=2023-06-05T17%3A06%3A28.235Z"></a>
+                    </div>
+                    <ul id="liste1">
+                        <li id="accueil"><a href="accueil">Accueil</a></li>
+                        <li id="reseau"><a href="reseau">Mon réseau</a></li>
+                        <li id="vous"><a href="vous">Vous</a></li>
+                        <li id="notifs"><a href="notifications">Notifications</a></li>
+                        <li id="emploisnav"><a href="emplois">Emplois</a></li>
+                        <li id="chat"><a href="chat">Messagerie</a></li>
+                        <li id="admin"><a href="admin">Admin</a></li>
+                        <li id="auteur"><a href="auteur">Auteur</a></li>
+                        <li id="deco"><a href="index">Deconnexion</a></li>
+                    </ul>
+                </nav>
             </div>
-            <ul id="liste1">
-                <li id="accueil"><a href="accueil">Accueil</a></li>
-                <li id="reseau"><a href="reseau">Mon réseau</a></li>
-                <li id="vous"><a href="vous">Vous</a></li>
-                <li id="notifs"><a href="notifications">Notifications</a></li>
-                <li id="emploisnav"><a href="emplois">Emplois</a></li>
-                <li id="chat"><a href="chat">Messagerie</a></li>
-                <li id="admin"><a href="admin">Admin</a></li>
-                <li id="auteur"><a href="auteur">Auteur</a></li>
-                <li id="deco"><a href="index">Deconnexion</a></li>
-            </ul>
-            </nav>
         </div>
-    </div>
     </div>
 </footer>
