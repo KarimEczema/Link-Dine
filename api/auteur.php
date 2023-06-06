2d368e140b9efb2c83d58799d3a388809d5fbe48
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 02:10:02 +0200

    nav fix

--- api/auteur.php
+++ api/auteur.php
@@ -8,9 +8,8 @@ echo '<title>Auteur</title>';
 
 // Here, we're adding the links to Bootstrap CSS and jQuery via their CDNs
 echo '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>';
-echo "Logged in as: " . $iduser;
-echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'; 
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>'; 
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>';
 echo '<link rel="stylesheet" type="text/css" href="css/auteur.css">';
 echo '<link rel="stylesheet" type="text/css" href="css/carrousel.css">';
 echo '<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1/dist/umd/supabase.min.js"></script>';
@@ -18,85 +17,89 @@ echo '</head>';
 echo '<body>';
 
 ?>
-    <nav class = "bg">
-<?php
-include 'navbar.php';
-?>
+<nav class="bg">
+    <?php
+    include 'navbar.php';
+    ?>
 
-<nav class = "post" style =" background-color: cyan;">
-    <form method="post" action="" enctype="multipart/form-data">
-        <label for="ameliorer">Creer un post</label><br>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-7"><textarea name="write" id="write" cols = "50" rows = "10" wrap="hard" required></textarea></div>
-                <div class="col-sm-5">
-                    <label for="image_uploads"><img id="preview" src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/Photo_site.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvUGhvdG9fc2l0ZS5wbmciLCJpYXQiOjE2ODU2NTA2OTIsImV4cCI6MTY4NjI1NTQ5Mn0.8V7VO2OmDmNFaN6lwNzgsw0zp_qBRhgorvFpWzmQDfc&t=2023-06-01T20%3A18%3A11.492Z" width="120" height="100" alt="Appareil photo . png">
-                    </label>
-                    <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" onchange="previewImage();" style="display:none">
-                    <button type="radio" id="publish_button" style = "margin-top : 10%; margin-left : 3%;">Publier</button>
-                    <fieldset>
-                        <p>A qui voulez vous le partager ?</p>
-                        <div>
-                            <input type="radio" id="friend" name="secu" value="friend" checked>
-                        <label for="huey">Vos amis</label>
-                        </div>
+    <nav class="post" style=" background-color: cyan;">
+        <form method="post" action="" enctype="multipart/form-data">
+            <label for="ameliorer">Creer un post</label><br>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-7"><textarea name="write" id="write" cols="50" rows="10" wrap="hard"
+                            required></textarea></div>
+                    <div class="col-sm-5">
+                        <label for="image_uploads"><img id="preview"
+                                src="https://bmqgiyygwjnnfyrtjkno.supabase.co/storage/v1/object/sign/Images/Photo_site.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvUGhvdG9fc2l0ZS5wbmciLCJpYXQiOjE2ODU2NTA2OTIsImV4cCI6MTY4NjI1NTQ5Mn0.8V7VO2OmDmNFaN6lwNzgsw0zp_qBRhgorvFpWzmQDfc&t=2023-06-01T20%3A18%3A11.492Z"
+                                width="120" height="100" alt="Appareil photo . png">
+                        </label>
+                        <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png"
+                            onchange="previewImage();" style="display:none">
+                        <button type="radio" id="publish_button"
+                            style="margin-top : 10%; margin-left : 3%;">Publier</button>
+                        <fieldset>
+                            <p>A qui voulez vous le partager ?</p>
+                            <div>
+                                <input type="radio" id="friend" name="secu" value="friend" checked>
+                                <label for="huey">Vos amis</label>
+                            </div>
 
-                        <div>
-                            <input type="radio" id="all" name="secu" value="all">
-                            <label for="dewey">Tout le monde</label>
-                        </div>
-                    </fieldset>
+                            <div>
+                                <input type="radio" id="all" name="secu" value="all">
+                                <label for="dewey">Tout le monde</label>
+                            </div>
+                        </fieldset>
+                    </div>
                 </div>
             </div>
-        </div>
-        <label for="start">Quand ?</label>
-        <input type="date" id="date" name="date" value="2023-06-06" min="2015-01-01" max="2026-12-31" style = "text-align : left">
-        <label for="where"style = "text-align : right;">Où ?</label>
-        <input type="text" id="lieu" name="lieu" style = "margin-left : 10%;">
-        <input type="hidden" id="image_url" name="image_url">
-        
-    </form>
-</nav>
-<script type="module" src="js/upload.js"></script>
+            <label for="start">Quand ?</label>
+            <input type="date" id="date" name="date" value="2023-06-06" min="2015-01-01" max="2026-12-31"
+                style="text-align : left">
+            <label for="where" style="text-align : right;">Où ?</label>
+            <input type="text" id="lieu" name="lieu" style="margin-left : 10%;">
+            <input type="hidden" id="image_url" name="image_url">
 
-<?php
+        </form>
+    </nav>
+    <script type="module" src="js/upload.js"></script>
 
-$dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
+    <?php
 
-try{
-    if($_POST)
-    {
+    $dsn = "pgsql:host=db.bmqgiyygwjnnfyrtjkno.supabase.co;port=5432;dbname=postgres;user=postgres;password=Au5SebXYkT3DUnW4";
 
-        $conn = new PDO($dsn);
-        // get the URL of the uploaded file from the hidden input
-        $photo = $_POST['image_url'];
-        
-    
-        //On définit certaines variables.
-        $ecriture = $_POST['write'];
-        $lieu = $_POST['lieu'];
-        $date = $_POST['date'];
-        $secu = $_POST['secu'];
-        
+    try {
+        if ($_POST) {
 
-        //On insère les données reçues
-        $sql = "INSERT INTO posts(descriptionpost, iduser, lieu, date, accessibilite, photo) VALUES(:write, :personne, :lieu, :date, :secu, :photo)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':write',$ecriture);
-        $stmt->bindParam(':personne',$iduser);
-        $stmt->bindParam(':lieu',$lieu);
-        $stmt->bindParam(':date',$date);
-        $stmt->bindParam(':secu',$secu);
-        $stmt->bindParam(':photo',$photo); 
-        $stmt->execute();
+            $conn = new PDO($dsn);
+            // get the URL of the uploaded file from the hidden input
+            $photo = $_POST['image_url'];
 
-        //Message de confirmation pour l'utilisateur
-        echo "Post publié !";
+
+            //On définit certaines variables.
+            $ecriture = $_POST['write'];
+            $lieu = $_POST['lieu'];
+            $date = $_POST['date'];
+            $secu = $_POST['secu'];
+
+
+            //On insère les données reçues
+            $sql = "INSERT INTO posts(descriptionpost, iduser, lieu, date, accessibilite, photo) VALUES(:write, :personne, :lieu, :date, :secu, :photo)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':write', $ecriture);
+            $stmt->bindParam(':personne', $iduser);
+            $stmt->bindParam(':lieu', $lieu);
+            $stmt->bindParam(':date', $date);
+            $stmt->bindParam(':secu', $secu);
+            $stmt->bindParam(':photo', $photo);
+            $stmt->execute();
+
+            //Message de confirmation pour l'utilisateur
+            echo "Post publié !";
+        }
+    } catch (PDOException $e) {
+        echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
     }
-}
-catch(PDOException $e){
-    echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
-}
 
-?>
-<?php include 'foot.php'; ?>
+    ?>
+    <?php include 'foot.php'; ?>
