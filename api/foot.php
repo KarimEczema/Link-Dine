feaad69c300609ff1e036fbf4c5be51e81d6d7ee
KarimEczema <97537899+KarimEczema@users.noreply.github.com>
2023-06-06 04:02:28 +0200

    FOOTER

--- api/foot.php
+++ api/foot.php
@@ -1,20 +1,41 @@
 <footer class="footer-navigation">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-12">
                 <p>
                     Bienvenue sur Link dine, le plus grand réseau professionnel mondial comptant plus de 2 utilisateurs
                     dans plus de 0 pays et territoires du monde.
                 </p>
             </div>
             <div class="col-sm-6">
-                <p>Nous contacter</p>
-                <a href="mailto:adrienne.vidon@edu.ece.fr"> Mail </a>
-                <br>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3661096301935!2d2.2859856116549255!3d48.851228701091536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20-%20Ecole%20d&#39;ing%C3%A9nieurs%20-%20Engineering%20school.!5e0!3m2!1sfr!2sfr!4v1685461093343!5m2!1sfr!2sfr"
-                    width="100" height="100" style="border:0;" loading="lazy"
+                    width="100%" height="100%" style="border:0;" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-sm-6">
+                <p>Nous contacter</p>
+                <a href="mailto:adrienne.vidon@edu.ece.fr"> Mail </a>
+                <a href="https://www.facebook.com/ECE.Paris/" target="_blank"> Facebook </a>
+                <a href="https://www.instagram.com/eceparis/" target="_blank"> Instagram </a>
+                <a href="https://twitter.com/ECEParis" target="_blank"> Twitter </a>
+                <a href="https://www.linkedin.com/school/ece-paris/" target="_blank"> LinkedIn </a>
+                <a href="https://www.ece.fr/" target="_blank"> Website </a>
+            </div>
+            <div class="col-sm-12">
+                <nav class="navigation">
+                    <ul id="liste1">
+                        <li id="accueil"><a href="accueil">Accueil</a></li>
+                        <li id="reseau"><a href="reseau">Mon réseau</a></li>
+                        <li id="vous"><a href="vous">Vous</a></li>
+                        <li id="notifs"><a href="notifications">Notifications</a></li>
+                        <li id="emploisnav"><a href="emplois">Emplois</a></li>
+                        <li id="chat"><a href="chat">Messagerie</a></li>
+                        <li id="admin"><a href="admin">Admin</a></li>
+                        <li id="auteur"><a href="auteur">Auteur</a></li>
+                        <li id="deco"><a href="index">Deconnexion</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
